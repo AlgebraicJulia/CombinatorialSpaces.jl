d2f0d988121c1aca01b95ba2f95178589c412427
bosonbaas <bosonbaas@gmail.com>
2022-01-18 15:57:37 -0500

    Added first draft of PD Wedge product

--- src/DiscreteExteriorCalculus.jl
+++ src/DiscreteExteriorCalculus.jl
@@ -497,6 +497,225 @@ function ∧(::Type{Tuple{1,1}}, s::HasDeltaSet2D, α, β, x::Int)
                       α[e1] * β[e0] - α[e0] * β[e1])) / 2
 end
 
+# Primal-Dual Wedge Products
+
+function edge_to_support(s)
+  vals = Dict{Tuple{Int64, Int64}, Float64}()
+  I = Vector{Int64}()
+  J = Vector{Int64}()
+  V = Vector{Float64}()
+  for e in 1:ne(s)
+      de = elementary_duals(Val{1},s, e)
+      ev = volume(Val{1}, s, e)
+      dv = sum([dual_volume(Val{1}, s, d) for d in de])
+      for d in de
+          dt = incident(s, d, :D_∂e0)
+          append!(I, dt)
+          append!(J, fill(e, length(dt)))
+          append!(V, fill(1/(dv*ev), length(dt)))
+      end
+  end
+  sparse(I,J,V)
+end
+
+function tri_to_support(s)
+  vals = Dict{Tuple{Int64, Int64}, Float64}()
+  I = Vector{Int64}()
+  J = Vector{Int64}()
+  V = Vector{Float64}()
+  for t in 1:ntriangles(s)
+      dt = vcat(incident(s, incident(s, triangle_center(s, t), :D_∂v0), :D_∂e1)...)
+      tv = volume(Val{2}, s, t)
+      append!(I, dt)
+      append!(J, fill(t, length(dt)))
+      append!(V, fill(1/tv#= * sign(Val{2}, s, t)=#, length(dt)))
+  end
+  sparse(I,J,V)
+end
+
+function support_to_tri(s)
+  vals = Dict{Tuple{Int64, Int64}, Float64}()
+  I = Vector{Int64}()
+  J = Vector{Int64}()
+  V = Vector{Float64}()
+  for t in 1:nv(s)
+      dt = elementary_duals(Val{0},s, t)
+      for d in dt
+          push!(I, t)
+          push!(J, d)
+          push!(V, 1)
+      end
+  end
+  sparse(I,J,V)
+end
+
+function support_edge_orient(s)
+  # Dictionary of rel_o[edge][triangle] = sign
+  rel_o = Vector{Dict{Int64, Int64}}(undef, ne(s))
+  for o in 1:length(rel_o)
+      rel_o[o] = Dict{Int64, Int64}()
+  end
+  for t in 1:ntriangles(s)
+      edges = triangle_edges(s,t)
+      rel_o[edges[1]][t] = sign(2,s,t) * sign(1,s,edges[1])
+      rel_o[edges[2]][t] = #=-1 * =#sign(2,s,t) * sign(1,s,edges[2])
+      rel_o[edges[3]][t] = sign(2,s,t) * sign(1,s,edges[3])
+  end
+  # Calculate edge for each support edge
+  s2e = zeros(Int64, nparts(s, :DualTri))
+  for e in 1:ne(s)
+      de = elementary_duals(Val{1},s, e)
+      for d in de
+          dt = incident(s, d, :D_∂e0)
+          s2e[dt] .= e
+      end
+  end
+  # Calculate tri for each support edge
+  s2t = zeros(Int64, nparts(s, :DualTri))
+  for t in 1:ntriangles(s)
+      dt = vcat(incident(s, incident(s, triangle_center(s, t), :D_∂v0), :D_∂e1)...)
+      s2t[dt] .= t
+  end
+  # Calculate rel_o for each support edge
+  s_orient = zeros(Int64, nparts(s, :DualTri))
+
+  for i in 1:length(s2t)
+      s_orient[i] = rel_o[s2e[i]][s2t[i]]
+  end
+  @show s_orient[1:10]
+  s_orient
+end
+
+function support_to_edge(s)
+  vals = Dict{Tuple{Int64, Int64}, Float64}()
+  I = Vector{Int64}()
+  J = Vector{Int64}()
+  V = Vector{Float64}()
+  s_orient = support_edge_orient(s)
+  for e in 1:ne(s)
+      de = elementary_duals(Val{1},s, e)
+      for d in de
+          dt = incident(s, d, :D_∂e0)
+          append!(J, dt)
+          append!(I, fill(e, length(dt)))
+          append!(V, fill(1, length(dt)) .* s_orient[dt])
+      end
+  end
+  sparse(I,J,V)
+end
+
+diag_vols(s) = spdiagm([dual_volume(Val{2}, s, dt) for dt in 1:nparts(s, :DualTri)])
+
+wedge_mat(::Type{Val{(1,1)}}, s) = 2.0 * (support_to_tri(s)*diag_vols(s)*edge_to_support(s))
+wedge_mat(::Type{Val{(2,0)}}, s) = support_to_tri(s)*diag_vols(s)*tri_to_support(s)
+
+#wedge_edge(::Type{Val{(1,1)}}, s) = 0.5 * (support_to_edge(s)*diag_vols(s)*edge_to_support(s))
+#wedge_edge(::Type{Val{(2,0)}}, s) = support_to_edge(s)*diag_vols(s)*tri_to_support(s)
+
+wedge_edge(::Type{Val{(1,1)}}, s) = dual_boundary(Val{2}, s) * 2.0 * (support_to_tri(s)*diag_vols(s)*edge_to_support(s))
+wedge_edge(::Type{Val{(2,0)}}, s) = dual_boundary(Val{2}, s) * support_to_tri(s)*diag_vols(s)*tri_to_support(s)
+
+function pd_wedge(::Type{Val{(1,1)}}, s, α, β; wedge_t = Dict((1,1)=>wedge_mat(Val{(1,1)}, s)), kw...)
+  wedge_t[(1,1)] * (α .* β)
+end
+
+function pd_wedge(::Type{Val{(2,0)}}, s, α, β; wedge_t = Dict((2,0)=>wedge_mat(Val{(2,0)},s)), kw...)
+  wedge_t[(2,0)] * (α .* β)
+end
+
+function pd_wedge(::Type{Val{(0,2)}}, s, α, β; wedge_t = nothing, kw...)
+  α .* β
+end
+
+# This might instead relate the calculated support volumes to the edges. It seems important that the resulting
+# dimension is a dual 1
+function pd_wedge(::Type{Val{(1,0)}}, s, α, β; d_mat = Dict(:d₁=>d(Val{1}, s), :dual_d₀=>dual_derivative(Val{0}, s)),
+                                             wedge_e = Dict((1,1)=>wedge_edge(Val{(1,1)},s),
+                                                            (2,0)=>wedge_edge(Val{(2,0)},s)), kw...)
+  #=(wedge_e[(2,0)] * ((d_mat[:d₁] * α) .* β)) -1 *=# (wedge_e[(1,1)] * (α .* (d_mat[:dual_d₀] * β)))
+  zeros(ne(s))
+end
+function init_wedge_ops(s)
+  (d_mat=Dict(:d₁=>d(Val{1}, s), :dual_d₀=>dual_derivative(Val{0}, s)),
+   wedge_e=Dict((1,1)=>wedge_edge(Val{(1,1)},s), (2,0)=>wedge_edge(Val{(2,0)},s)),
+   wedge_t=Dict((1,1)=>wedge_mat(Val{(1,1)}, s), (2,0)=>wedge_mat(Val{(2,0)},s)))
+end
+
+vect(s, e) = (s[s[e,:tgt], :point] - s[s[e,:src], :point]) * sign(1, s, e)
+vect(s, e::AbstractVector) = [vect(s, el) for el in e]
+t_vects(s,t) = vect(s, triangle_edges(s,t)) .* ([1,-1,1] * sign(2,s,t))
+function comp_support(sd)
+  vects = []
+  for t in 1:ntriangles(sd)
+      inds = triangle_edges(sd, t)
+      for i in 1:3
+          push!(vects, (t, inds[i]))
+      end
+  end
+  v2comp = Dict{Tuple{Int64, Int64}, Int64}()
+  for (i, v) in enumerate(vects)
+      v2comp[v] = i
+  end
+  v2comp
+end
+function changes(s, v2comp)
+  orient_vals = [1,-1,1]
+  I = Vector{Int64}()
+  J = Vector{Int64}()
+  V = Vector{Float64}()
+  for t in 1:ntriangles(s)
+      inds = triangle_edges(s, t)
+      e_vects = t_vects(s,t)
+      vals = zeros(1:3)
+      for i in 1:3
+          ns = [(i+1)%3 + 1, i%3+1]
+          ort = e_vects[i] × (e_vects[i] × e_vects[ns[1]])
+          n_ort = ort / norm(ort)
+          append!(J, v2comp[(t,inds[i])])
+          append!(I, inds[ns[1]])
+          append!(V, dot(n_ort, e_vects[ns[1]]) * orient_vals[ns[1]] * sign(1, s, ns[1])* orient_vals[i]* sign(2,s,t) / 3.0)
+          append!(J, v2comp[(t,inds[i])])
+          append!(I, inds[ns[2]])
+          append!(V, dot(n_ort, e_vects[ns[2]]) * orient_vals[ns[2]] * sign(1, s, ns[2])* orient_vals[i]* sign(2,s,t) / 3.0)
+      end
+  end
+  sparse(I,J,V, ne(s), ntriangles(s)*3)
+end
+function edge2comp(s, v2comp)
+  I = Vector{Int64}()
+  J = Vector{Int64}()
+  V = Vector{Float64}()
+  for t in 1:ntriangles(sd)
+      inds = triangle_edges(sd, t)
+      for i in 1:3
+          push!(I, v2comp[(t,inds[i])])
+          push!(J, inds[i])
+          push!(V, 1 / volume(Val{1}, s, inds[i]))
+      end
+  end
+  sparse(I,J,V)
+end
+function tri2comp(s, v2comp)
+  I = Vector{Int64}()
+  J = Vector{Int64}()
+  V = Vector{Float64}()
+  for t in 1:ntriangles(sd)
+      inds = triangle_edges(sd, t)
+      for i in 1:3
+          push!(I, v2comp[(t,inds[i])])
+          push!(J, t)
+          push!(V, 1)
+      end
+  end
+  sparse(I,J,V)
+end
+
+function cp_2_1(α, β, matrices)
+  matrices[:cross] * ((matrices[:t2c]*α).*(matrices[:e2c]*β))
+end
+
+## Done
+
 function subdivide_duals!(s::EmbeddedDeltaDualComplex2D, args...)
   subdivide_duals_2d!(s, args...)
   precompute_volumes_2d!(s)
