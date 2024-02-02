0ba6d57f3982ac7365431b17385c51a8f74c5424
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2024-02-02 10:56:30 -0500

    Added FastDEC (#64)
    * Added FastDEC
    
    These are some fundamental DEC operators that have been optimized for the DeltaSet ACSets.
    
    * Added tests for FastDEC
    
    These tests are adapted from the tests for DiscreteExteriorCalculus.
    
    * Shifted over operators code
    
    Shifted over all of the supporting code for the fast operators. This can probably be more organized but as it stands this allows the new operators to work, along with their tests and benchmarks.
    
    * Cleaned up brought over code
    
    Smashed mesh generation code into one single file, Meshes.jl, and brought over the Meshes test file from decapodes.
    
    * Removed todo in main
    
    * Added modules around new tests
    
    * Cleaning up
    
    Still should add docstrings for other fast operators. Also still need to look at improving Hodge 1 for 2D.
    
    * Edited hodges to use stricter mesh type
    
    * Improvements to GeoHdg1
    
    Also switched other operators to accept float_types from mesh itself, not as a keyword.
    
    * Final improvements to Geo Hodge 1
    
    * Editted wedge docstrings
    
    * Export wedge products with @GeorgeR227
    
    ---------
    
    Co-authored-by: Luke Morris <lukelukemorrismorris@gmail.com>

--- src/CombinatorialSpaces.jl
+++ src/CombinatorialSpaces.jl
@@ -9,10 +9,14 @@ include("ExteriorCalculus.jl")
 include("SimplicialSets.jl")
 include("DiscreteExteriorCalculus.jl")
 include("MeshInterop.jl")
+include("FastDEC.jl")
+include("Meshes.jl")
 
 @reexport using .SimplicialSets
 @reexport using .DiscreteExteriorCalculus
+@reexport using .FastDEC
 @reexport using .MeshInterop
+@reexport using .Meshes
 
 function __init__()
   @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" include("MeshGraphics.jl")
