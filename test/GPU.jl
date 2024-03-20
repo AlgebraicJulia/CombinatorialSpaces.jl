fc1497dbd889033ed11ddb8cf7ecf1df74ab5525
JoshHKim <71045661+JoshHKim@users.noreply.github.com>
2024-03-20 19:57:38 -0400

    Added GPU.jl under tests to test package extension

---
+++ test/GPU.jl
@@ -0,0 +1,22 @@
+module testGPU
+
+using Test
+using CombinatorialSpaces
+using Pkg
+
+package_name = "CUDA"
+is_cuda_installed = false
+
+try
+    using CUDA
+    is_cuda_installed = true
+catch e
+    is_cuda_installed = false
+end
+
+if is_cuda_installed
+    @testset "Conditional Loading of WedgeGPU.jl" begin
+            @test isdefined(CombinatorialSpaces, dec_cu_c_wedge_product)
+        end
+    end
+end
