084a6f9ec30056cddec7b46ad6728711f3d37e9a
Luke Morris <70283489+lukem12345@users.noreply.github.com>
2024-04-25 13:05:33 -0400

    Use CUDA.functional() to check GPU support

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,18 +21,14 @@ end
   include("MeshGraphics.jl")
 end
 
-try
-  using CUDA
-catch exception
-  @info "CUDA loading failed."
-end
-
-if(!isnothing(Base.get_extension(RunTests, :CombinatorialSpacesCUDAExt)))
+using CUDA
+if CUDA.functional()
   @testset "CUDA" begin
     include("OperatorsCUDA.jl")
   end
 else
   @info "CUDA tests were not run."
+  @info CUDA.functional(true)
 end
 
 end
