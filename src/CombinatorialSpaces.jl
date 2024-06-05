36dce571eec832b3be07057f0b2d4f7f7d843943
Owen Lynch <root@owenlynch.org>
2024-06-05 11:45:39 -0700

    Added basic simplicial complexes, vendored trie implementation
    Co-authored-by: Kevin Carlson <kevin@topos.institute>

--- src/CombinatorialSpaces.jl
+++ src/CombinatorialSpaces.jl
@@ -2,17 +2,21 @@ module CombinatorialSpaces
 
 using Reexport
 
+include("Tries.jl")
 include("ArrayUtils.jl")
 include("CombinatorialMaps.jl")
 include("ExteriorCalculus.jl")
 include("SimplicialSets.jl")
 include("DiscreteExteriorCalculus.jl")
 include("MeshInterop.jl")
+include("SimplicialComplexes.jl")
 include("FastDEC.jl")
 include("Meshes.jl")
 include("restrictions.jl")
 
+@reexport using .Tries
 @reexport using .SimplicialSets
+@reexport using .SimplicialComplexes
 @reexport using .DiscreteExteriorCalculus
 @reexport using .FastDEC
 @reexport using .MeshInterop
