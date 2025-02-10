22d7ca9d3ec9aa0d378034161bad8bae1ece2439
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-02-10 11:47:47 -0500

    Added solver wrapper for use with ldiv!
    In-place method can now work as a preconditioner for krylov methods.

--- src/fast_mg.jl
+++ src/fast_mg.jl
@@ -3,6 +3,7 @@ using GeometryBasics: Point3, Point2
 using Random
 using BenchmarkTools
 using SparseArrays
+using KrylovPreconditioners
 const Point3D = Point3{Float64}
 const Point2D = Point2{Float64}
 
@@ -16,31 +17,21 @@ L = first(md.operators)
 Random.seed!(0)
 b = L*rand(nv(sd))
 
-"""
-Access the leading grid, restriction, prolongation, and step radius at the desired index of the multigrid data list.
-"""
-function mg_level(md::MultigridData, idx::Int)
-  if idx == length(md)
-    return md.operators[idx],nothing,nothing,md.steps[idx]
-  end
-  return md.operators[idx],md.restrictions[idx],md.prolongations[idx],md.steps[idx]
-end
-
-struct MultiGridCache{F, S}
-  zeros_list::Vector{Vector{F}}
-  delta_list::Vector{Vector{F}}
+struct MultigridCache{F, S}
+  zeros::Vector{Vector{F}}
+  delta::Vector{Vector{F}}
   res_checkout::Vector{Bool}
-  res_list::Vector{Vector{F}}
-  solver_list::Vector{S}
+  res::Vector{Vector{F}}
+  solver::Vector{S}
 end
 
 # Only cache will change, Krylov doesn't change warm start value
-function test_multigrid_v_cycle!(cache::MultiGridCache, md::MultigridData, alg!, idx, x_0, b)
+function test_multigrid_v_cycle!(cache::MultigridCache, md::MultigridData, alg!, idx, x0, b)
   A = md.operators[idx]
   steps = md.steps[idx]
-  solver = cache.solver_list[idx]
+  solver = cache.solver[idx]
 
-  pre_smooth = alg!(solver, A, b, x_0; itmax=steps)
+  pre_smooth = alg!(solver, A, b, x0; itmax=steps)
   x_h = solution(pre_smooth)
   if idx == length(md)
     return x_h
@@ -56,15 +47,15 @@ function test_multigrid_v_cycle!(cache::MultiGridCache, md::MultigridData, alg!,
   cache.res_checkout[idx+1] = true
 
   r_h = pre_smooth.r
-  r_H = cache.res_list[idx+1]
+  r_H = cache.res[idx+1]
   mul!(r_H, R_map, r_h)
 
-  δ0_H = cache.zeros_list[idx + 1]
+  δ0_H = cache.zeros[idx + 1]
   δ_H = test_multigrid_v_cycle!(cache, md, alg!, idx + 1, δ0_H, r_H)
 
   cache.res_checkout[idx+1] = false
 
-  δ_h = cache.delta_list[idx]
+  δ_h = cache.delta[idx]
   mul!(δ_h, P_map, δ_H)
   x_h .= x_h .+ δ_h
 
@@ -74,18 +65,25 @@ function test_multigrid_v_cycle!(cache::MultiGridCache, md::MultigridData, alg!,
   return x_h
 end
 
-function run_test_multigrid_v_cycle!(md::MultigridData, alg!, cycles, x_0, b)
-  zeros_list = map(op->zeros(size(op, 1)), md.operators)
-  delta_list = map(op->zeros(size(op, 1)), md.operators)
+# GPU support?
+# TODO: Check alt-float support
+function generate_mg_cache(md::MultigridData, kr_solver, float_type::DataType = Float64)
+  zero = map(op->zeros(float_type, size(op, 1)), md.operators)
+  delta = map(op->zeros(float_type, size(op, 1)), md.operators)
   res_checkout = fill(false, length(md))
-  res_list = map(op->zeros(size(op, 1)), md.operators)
-  solver_list = map(op->CgSolver(size(op)..., Vector{Float64}), md.operators) # TODO: Make type user settable
-  solver_type = typeof(first(solver_list))
-  cache = MultiGridCache{Float64, solver_type}(zeros_list, delta_list, res_checkout, res_list, solver_list)
+  res = map(op->zeros(float_type, size(op, 1)), md.operators)
+  solver = map(op->kr_solver(size(op)..., Vector{float_type}), md.operators)
+  solver_type = typeof(first(solver))
 
-  x = x_0
+  MultigridCache{Float64, solver_type}(zero, delta, res_checkout, res, solver)
+end
+
+cache = generate_mg_cache(md, CgSolver, Float64);
+
+function run_test_multigrid_v_cycle!(cache::MultigridCache, md::MultigridData, alg!, cycles, x0, b)
+  x = deepcopy(x0)
   for i in 1:cycles
-    x = test_multigrid_v_cycle!(cache, md, alg!, 1, x, b)
+    x .= test_multigrid_v_cycle!(cache, md, alg!, 1, x, b)
   end
 
   return x
@@ -98,10 +96,42 @@ x0 = zeros(nv(sd));
 test_x = multigrid_vcycles(x0, b, md, 25);
 res(L, test_x, b)
 
-in_mg_x = run_test_multigrid_v_cycle!(md, cg!, 25, x0, b);
+in_mg_x = run_test_multigrid_v_cycle!(cache, md, cg!, 25, x0, b);
 res(L, in_mg_x, b)
 
-@time run_test_multigrid_v_cycle!(md, cg!, 25, x0, b);
+@time run_test_multigrid_v_cycle!(cache, md, cg!, 25, x0, b);
 @time multigrid_vcycles(x0, b, md, 25);
 
-# cg_solver = CgSolver(L, b)
+struct MultiGridSolverKit
+  md::MultigridData
+  cache::MultigridCache
+  alg
+  x0::AbstractVector
+  cycles::Int
+end
+
+function MultiGridSolverKit(md::MultigridData, kr_solver, float_type::DataType, kr_alg, x0::AbstractVector, cycles::Int)
+  return MultiGridSolverKit(md, generate_mg_cache(md, kr_solver, float_type), kr_alg, x0, cycles)
+end
+
+mg = MultiGridSolverKit(md::MultigridData, CgSolver, Float64, cg!, zeros(nv(sd)), 2);
+
+import LinearAlgebra: ldiv!
+function ldiv!(y, A::MultiGridSolverKit, b)
+  y .= run_test_multigrid_v_cycle!(A.cache, A.md, A.alg, A.cycles, A.x0, b);
+end
+
+println("Geometric Multigrid Preconditioned CG")
+kr_mg_u, kr_mg_stats = cg(L, b, M = mg, ldiv = true, history = true);
+@show kr_mg_stats.timer
+@show last(kr_mg_stats.residuals)
+
+println("Incomplete LU Preconditioned CG")
+ilu0_L = ilu(L);
+kr_u, kr_stats = cg(L, b, M = ilu0_L, ldiv = true, history = true);
+@show kr_stats.timer
+@show last(kr_stats.residuals)
+
+println("Direct Solve")
+splu_L = lu(L);
+@time ds_u = splu_L \ b;
