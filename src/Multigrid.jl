4bc37f1e03d1451eda7064bed6c333dcab63815b
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-02-21 11:22:29 -0500

    Use StaticArrays in Binary Subdivision (#141)

--- src/Multigrid.jl
+++ src/Multigrid.jl
@@ -38,10 +38,11 @@ function binary_subdivision(s::EmbeddedDeltaSet2D)
   add_parts!(sd, :E, 2*ne(s)+3*ntriangles(s))
   for e in edges(s)
     offset = 2*e-1
-    mid = e+nv(s)
+    e_idxs = SVector{2}(offset, offset+1)
+    m = e+nv(s)
 
-    sd[offset:offset+1, :∂v0] = mid,        mid
-    sd[offset:offset+1, :∂v1] = s[e, :∂v0], s[e, :∂v1]
+    sd[e_idxs, :∂v0] = m,          m
+    sd[e_idxs, :∂v1] = s[e, :∂v0], s[e, :∂v1]
   end
 
   #       v2
@@ -50,6 +51,7 @@ function binary_subdivision(s::EmbeddedDeltaSet2D)
   #   /  \  /  \
   # v0 -- m2 -- v1
   add_parts!(sd, :Tri, 4*ntriangles(s))
+  inc_arr = SVector{3}(0,1,2)
   for t in triangles(s)
     es = triangle_edges(s,t)
 
@@ -57,19 +59,20 @@ function binary_subdivision(s::EmbeddedDeltaSet2D)
     m0, m1, m2 = es .+ nv(s)
 
     # Edge indices:
-    m0_m1, m1_m2, m0_m2 = (3t-2 + 2ne(s)) .+ [0,1,2]
+    m0_m1, m1_m2, m0_m2 = (3t-2 + 2ne(s)) .+ inc_arr
     m0_v1, m1_v0, m2_v0 = 2es
     v2_m0, v2_m1, v1_m2 = 2es .- 1
 
-    # Edge × Vertex:
-    sd[[m0_m1, m1_m2, m0_m2], :∂v0] = m1, m2, m2
-    sd[[m0_m1, m1_m2, m0_m2], :∂v1] = m0, m1, m0
+    # Vertex × Edge:
+    sd[SVector{3}(m0_m1, m1_m2, m0_m2), :∂v0] = m1, m2, m2
+    sd[SVector{3}(m0_m1, m1_m2, m0_m2), :∂v1] = m0, m1, m0
 
-    # Triangle × Edge:
+    # Edge × Triangle:
     offset = 4t-3
-    sd[offset:offset+3, :∂e0] = m1_m2, m1_m2, m0_m2, m0_m1
-    sd[offset:offset+3, :∂e1] = m0_m2, m2_v0, v1_m2, v2_m1
-    sd[offset:offset+3, :∂e2] = m0_m1, m1_v0, m0_v1, v2_m0
+    tri_idxs = SVector{4}(offset, offset+1, offset+2, offset+3)
+    sd[tri_idxs, :∂e0] = m1_m2, m1_m2, m0_m2, m0_m1
+    sd[tri_idxs, :∂e1] = m0_m2, m2_v0, v1_m2, v2_m1
+    sd[tri_idxs, :∂e2] = m0_m1, m1_v0, m0_v1, v2_m0
   end
   sd
 end
