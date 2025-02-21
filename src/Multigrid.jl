f58ca0ec4fd54d721f72f27412c39491dc6a9ab7
Luke Morris <70283489+lukem12345@users.noreply.github.com>
2025-02-21 11:19:34 -0500

    Update legacy variable names

--- src/Multigrid.jl
+++ src/Multigrid.jl
@@ -38,11 +38,11 @@ function binary_subdivision(s::EmbeddedDeltaSet2D)
   add_parts!(sd, :E, 2*ne(s)+3*ntriangles(s))
   for e in edges(s)
     offset = 2*e-1
-    offsets = SVector{2}(offset, offset+1)
-    mid = e+nv(s)
+    e_idxs = SVector{2}(offset, offset+1)
+    m = e+nv(s)
 
-    sd[offsets, :∂v0] = mid,        mid
-    sd[offsets, :∂v1] = s[e, :∂v0], s[e, :∂v1]
+    sd[e_idxs, :∂v0] = m,          m
+    sd[e_idxs, :∂v1] = s[e, :∂v0], s[e, :∂v1]
   end
 
   #       v2
@@ -63,16 +63,16 @@ function binary_subdivision(s::EmbeddedDeltaSet2D)
     m0_v1, m1_v0, m2_v0 = 2es
     v2_m0, v2_m1, v1_m2 = 2es .- 1
 
-    # Edge × Vertex:
+    # Vertex × Edge:
     sd[SVector{3}(m0_m1, m1_m2, m0_m2), :∂v0] = m1, m2, m2
     sd[SVector{3}(m0_m1, m1_m2, m0_m2), :∂v1] = m0, m1, m0
 
-    # Triangle × Edge:
+    # Edge × Triangle:
     offset = 4t-3
-    offsets = SVector{4}(offset, offset+1, offset+2, offset+3)
-    sd[offsets, :∂e0] = m1_m2, m1_m2, m0_m2, m0_m1
-    sd[offsets, :∂e1] = m0_m2, m2_v0, v1_m2, v2_m1
-    sd[offsets, :∂e2] = m0_m1, m1_v0, m0_v1, v2_m0
+    tri_idxs = SVector{4}(offset, offset+1, offset+2, offset+3)
+    sd[tri_idxs, :∂e0] = m1_m2, m1_m2, m0_m2, m0_m1
+    sd[tri_idxs, :∂e1] = m0_m2, m2_v0, v1_m2, v2_m1
+    sd[tri_idxs, :∂e2] = m0_m1, m1_v0, m0_v1, v2_m0
   end
   sd
 end
