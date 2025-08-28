1e6f1987b81de95fc337637b4348a456841e8a51
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-08-28 17:38:23 -0400

    Cleaned up float_type retrieval

--- ext/CombinatorialSpacesDelaunayTriangulationExt.jl
+++ ext/CombinatorialSpacesDelaunayTriangulationExt.jl
@@ -3,10 +3,12 @@ using CombinatorialSpaces
 using DelaunayTriangulation
 
 import CombinatorialSpaces: EmbeddedDeltaSet2D
+import DelaunayTriangulation: number_type
 
 function EmbeddedDeltaSet2D(triangulation::T) where {T<:DelaunayTriangulation.Triangulation}
-  float_type = eltype(first(first(triangulation.points)))
+  float_type = number_type(triangulation.points)
   coords = map(p -> Point3{float_type}(p[1], p[2], 0), triangulation.points)
+
   s = EmbeddedDeltaSet2D{Bool, Point3{float_type}}()
   add_vertices!(s, length(coords), point=coords)
   for tri in each_solid_triangle(triangulation)
