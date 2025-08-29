0031339569bb2cd09b34d3a791ad475c28d5303d
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-08-29 14:22:49 -0400

    Added type conversion warning

--- ext/CombinatorialSpacesDelaunayTriangulationExt.jl
+++ ext/CombinatorialSpacesDelaunayTriangulationExt.jl
@@ -6,6 +6,7 @@ import CombinatorialSpaces: EmbeddedDeltaSet2D
 import DelaunayTriangulation: number_type
 
 function EmbeddedDeltaSet2D(triangulation::T) where {T<:DelaunayTriangulation.Triangulation}
+  @warn "EmbeddedDeltaSet2D mesh will use 3D points, setting z-coordinate to 0"
   float_type = number_type(triangulation.points)
   coords = map(p -> Point3{float_type}(p[1], p[2], 0), triangulation.points)
 
