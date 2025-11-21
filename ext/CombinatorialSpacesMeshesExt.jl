04710ff57d831639dc4487d037399e8dc53a40cf
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-11-21 12:56:10 -0500

    Add support for Meshes.jl interop (#187)
    * Started Meshes support
    
    * Fixed issue with name conflict
    
    Meshes.jl was both a package name (external) and a file name used internally in CombinatorialSpaces. This has been changed to CombMeshes.jl.
    
    * Added support for 2 and 3D point input
    
    This needs testing
    
    * Completed code
    
    Will need to see if file renaming was required
    
    * Replaced sort with set in tests

---
+++ ext/CombinatorialSpacesMeshesExt.jl
@@ -0,0 +1,36 @@
+module CombinatorialSpacesMeshesExt
+using CombinatorialSpaces
+using Meshes
+
+import CombinatorialSpaces: EmbeddedDeltaSet2D
+
+function EmbeddedDeltaSet2D(mesh::SimpleMesh)
+  # This code supports capturing both 2D and 3D input points
+  # @warn "EmbeddedDeltaSet2D mesh will use 3D points, setting z-coordinate to 0"
+
+  function numerical_coords(p)
+    c = getproperty.(getfield(p.coords, :coords), :val)
+    dim = length(c)
+    float_type = eltype(c)
+    if dim == 3
+      return Point3{float_type}(c)
+    elseif dim == 2
+      return Point3{float_type}(c..., 0)
+    else
+      @error "The mesh provided has points of unsupported dimension $dim"
+    end
+  end
+  cs = map(p -> numerical_coords(p), Meshes.vertices(mesh))
+
+  s = EmbeddedDeltaSet2D{Bool, typeof(first(cs))}()
+  add_vertices!(s, length(cs), point=cs)
+  for tri in mesh.topology.connec
+    length(tri.indices) == 3 || @error "Mesh contains non-triangular elements"
+    glue_sorted_triangle!(s, tri.indices...)
+  end
+  s[:edge_orientation] = false
+  orient!(s)
+  s
+end
+
+end
