a718dd5656214bd0a9cdf96f30064feafd38344f
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-12-12 16:16:25 -0500

    Added plotting code and relative orientations
    This adds support for mesh plotting through Makie and proper computation of exterior derivatives. Next steps will be dual mesh generation.

--- test/CubicalComplexes.jl
+++ test/CubicalComplexes.jl
@@ -1,7 +1,7 @@
 using Test
+using CairoMakie
 
 include("../src/CubicalComplexes.jl")
-
 s = EmbeddedCubicalComplex2D()
 
 add_vertex!(s, Point3d(0,0,0))
@@ -10,6 +10,11 @@ add_vertex!(s, Point3d(1,1,0))
 add_vertex!(s, Point3d(0,1,0))
 
 glue_quad!(s, 3, 4, 1, 2)
+
+fig = Figure();
+ax = CairoMakie.Axis(fig[1,1])
+wireframe!(ax, s)
+fig
 
 @test nv(s) == 4
 @test ne(s) == 4
@@ -20,16 +25,27 @@ glue_quad!(s, 3, 4, 1, 2)
 
 @test quad_area(s, 1) == 1.0
 
-@test s.∂v0 == [1,1,2,3]
-@test s.∂v1 == [2,4,3,4]
+@test s.∂v0 == [3,1,1,2]
+@test s.∂v1 == [4,4,2,3]
 
-@test edge_vertices(s, 1) == [1,2]
-@test quad_vertices(s, 1) == [3, 4, 1, 2]
+@test edge_vertices(s, 1) == [3,4]
 @test quad_edges(s, 1) == [1,2,3,4]
+@test quad_vertices(s, 1) == [3,4,1,2]
+@test quad_edge_orients(s, 1) == [1,-1,1,1]
 
 d0 = exterior_derivative(Val(0), s)
 d1 = exterior_derivative(Val(1), s)
 
 @test all(0 .== d1 * d0)
 
-grid = construct_grid(10, 10, 3, 3)
+grid = construct_grid(10, 10, 11, 11)
+
+fig = Figure();
+ax = CairoMakie.Axis(fig[1,1])
+wireframe!(ax, grid)
+fig
+
+d0 = exterior_derivative(Val(0), grid)
+d1 = exterior_derivative(Val(1), grid)
+
+@test all(0 .== d1 * d0)
