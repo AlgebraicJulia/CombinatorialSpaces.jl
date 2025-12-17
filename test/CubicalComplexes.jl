901742ba2003ea75c19db49584216af6124f1e08
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2025-12-17 17:00:53 -0500

    Added several DEC operators
    Dual wedge 01 needs to be added and further tests on operators need to be encoded. Also, built in support for periodic boundaries is needed.

--- test/CubicalComplexes.jl
+++ test/CubicalComplexes.jl
@@ -43,10 +43,37 @@ for q in quadrilaterals(s)
   @test all(1 .<= quad_edges(s, q) .<= ne(s))
 end
 
+for dq in vertices(s)
+  @test 25.0 == dual_quad_area(s, dq)
+end
+
 d0 = exterior_derivative(Val(0), s)
 d1 = exterior_derivative(Val(1), s)
 
 @test all(0 .== d1 * d0)
+
+dual_d0 = dual_derivative(Val(0), s)
+dual_d1 = dual_derivative(Val(1), s)
+
+@test all(0 .== dual_d1 * dual_d0)
+
+v1 = ones(nv(s))
+e1 = ones(ne(s))
+q1 = ones(nquads(s))
+
+all(1.0 .== wedge_product(Val((0,1)), s, v1, e1))
+all(1.0 .== wedge_product(Val((0,2)), s, v1, q1))
+
+all(0.0 .== wedge_product(Val((1,1)), s, e1, e1))
+all(0.0 .== wedge_product(Val((1,1)), s, e1, 10 * e1))
+
+eX = zeros(ne(s))
+eX[1:nhe(s)] .= 1
+
+eY = zeros(ne(s))
+eY[nhe(s)+1:ne(s)] .= 1
+all(1.0 .== wedge_product(Val((1,1)), s, eX, eY))
+all(-1.0 .== wedge_product(Val((1,1)), s, eY, eX))
 
 ps = Point3d[]
 for y in [0, 1, 3, 6, 10]
@@ -104,3 +131,67 @@ d0 = exterior_derivative(Val(0), s)
 d1 = exterior_derivative(Val(1), s)
 
 @test all(0 .== d1 * d0)
+
+v1 = ones(nv(s))
+e1 = ones(ne(s))
+q1 = ones(nquads(s))
+
+all(1.0 .== wedge_product(Val((0,1)), s, v1, e1))
+all(1.0 .== wedge_product(Val((0,2)), s, v1, q1))
+
+all(0.0 .== wedge_product(Val((1,1)), s, e1, e1))
+all(0.0 .== wedge_product(Val((1,1)), s, e1, 10 * e1))
+
+eX = zeros(ne(s))
+eX[1:nhe(s)] .= 1
+
+eY = zeros(ne(s))
+eY[nhe(s)+1:ne(s)] .= 1
+all(1.0 .== wedge_product(Val((1,1)), s, eX, eY))
+all(-1.0 .== wedge_product(Val((1,1)), s, eY, eX))
+
+dual_d0 = dual_derivative(Val(0), s)
+dual_d1 = dual_derivative(Val(1), s)
+
+@test all(0 .== dual_d1 * dual_d0)
+
+### HEAT EQUATION ###
+
+s = uniform_grid(10, 10, 3, 3);
+fig = Figure();
+ax = CairoMakie.Axis(fig[1,1])
+wireframe!(ax, s)
+fig
+
+u_0 = map(points(s)) do p
+  if (3 <= p[1] <= 7) && (3 <= p[2] <= 7)
+    return 1.0
+  else
+    return 0.0
+  end
+end
+
+fig = Figure();
+ax = CairoMakie.Axis(fig[1,1])
+mesh!(ax, s, color = u_0)
+Colorbar(fig[1,2])
+fig
+
+d0 = exterior_derivative(Val(0), s)
+
+plot_oneform(s, d0 * u_0)
+
+Δt = 0.001
+u = deepcopy(u_0)
+
+Δ0 = laplacian(Val(0), s) 
+
+for _ in 0:Δt:0.1
+  u .= u .+ Δt * Δ0 * u
+
+  fig = Figure();
+  ax = CairoMakie.Axis(fig[1,1])
+  mesh!(ax, s, color = u)
+  Colorbar(fig[1,2])
+  display(fig)
+end
