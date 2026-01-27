b594585e103d890a546ae056511d71c65879ed69
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2026-01-27 15:44:16 -0500

    Basic advection working

--- test/CubicalTests/WENO_Test.jl
+++ test/CubicalTests/WENO_Test.jl
@@ -1,15 +1,19 @@
 using Plots
+using Distributions
 
-half_nx = 50
-nx = half_nx * 2
+nx = 101
+hx = 1 / (nx - 1)
 
 xs = range(0, 1; length = nx)
 
-f = zeros(nx)
-f[1:half_nx] = map(x -> sin(2pi*x), xs[1:half_nx])
-f[half_nx+1:end] = map(x -> 1 - sin(2pi*x), xs[half_nx+1:end])
+# f = zeros(nx)
+# f[1:half_nx] = map(x -> sin(2pi*x), xs[1:half_nx])
+# f[half_nx+1:end] = map(x -> 1 - sin(2pi*x), xs[half_nx+1:end])
 
-scatter(xs, f)
+dist = Normal(0.5, 0.1)
+f_0 = map(x -> pdf(dist, x), xs)
+
+plot(xs, f_0)
 
 # Optimal coefficients for combined stencil, 5th order
 # Cr_k, r is order, k is stencil shift
@@ -33,9 +37,19 @@ a3_21 = 1/3
 a3_22 = 5/6
 a3_23 = -1/6
 
-f_hat = zeros(nx - 1)
-f_hat[3:end] = map(i -> a3_01 * f[i - 2] + a3_02 * f[i - 1] + a3_03 * f[i], 3:nx-1)
-f_hat[2:end] = map(i -> a3_11 * f[i - 1] + a3_12 * f[i] + a3_13 * f[i + 1], 2:nx-1)
-f_hat[1:end-1] = map(i -> a3_21 * f[i] + a3_22 * f[i + 1] + a3_23 * f[i + 2], 1:nx-2)
+f = deepcopy(f_0)
 
-scatter(xs, f_hat)
+t_e = 0.1
+Δt = hx
+
+function IS()
+
+for _ in 0:Δt:t_e
+  f_hat = zeros(nx - 1)
+  # f_hat[3:end] = map(i -> a3_01 * f[i - 2] + a3_02 * f[i - 1] + a3_03 * f[i], 3:nx-1)
+  f_hat[2:end] = map(i -> a3_11 * f[i - 1] + a3_12 * f[i] + a3_13 * f[i + 1], 2:nx-1)
+  # f_hat[1:end-1] = map(i -> a3_21 * f[i] + a3_22 * f[i + 1] + a3_23 * f[i + 2], 1:nx-2)
+  f[2:end-1] -= Δt * diff(f_hat) / hx
+end
+
+plot(xs, f)
