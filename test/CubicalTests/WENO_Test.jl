2c9d945468827e0340dd700f1e33387601410c2f
GeorgeR227 <78235421+GeorgeR227@users.noreply.github.com>
2026-01-28 16:57:53 -0500

    Completed WENO simulation in 1D
    This incorporates the smoothness indicators as part of the computation.

--- test/CubicalTests/WENO_Test.jl
+++ test/CubicalTests/WENO_Test.jl
@@ -1,13 +1,14 @@
 using Plots
 using Distributions
+using OrdinaryDiffEq
 
-nx = 201
+nx = 301
 lx = 2
 hx = lx / (nx - 1)
 
 xs = range(0, lx; length = nx)
 
-dist = Normal(0.3, 0.05)
+dist = Normal(0.3, 0.01)
 f_0 = map(x -> pdf(dist, x), xs)
 
 plot(xs, f_0)
@@ -34,26 +35,33 @@ a3_21 = 1/3
 a3_22 = 5/6
 a3_23 = -1/6
 
-IS0(f, j::Int) = 13/12 *(f[j-2]-2f[j-1]+f[j])^2 + 1/4*(f[j-2]-4f[j-1]+3f[j])^2
-IS1(f, j::Int) = 13/12 *(f[j-1]-2f[j]+f[j+1])^2 + 1/4*(f[j-1]-f[j+1])^2
-IS2(f, j::Int) = 13/12 *(f[j]-2f[j+1]+f[j+2])^2 + 1/4*(3f[j]-4f[j+1]+f[j+2])^2
+IS0(f, j::Int) = 13/12*(f[j-2]-2f[j-1]+f[j])^2 + 1/4*(f[j-2]-4f[j-1]+3f[j])^2
+IS1(f, j::Int) = 13/12*(f[j-1]-2f[j]+f[j+1])^2 + 1/4*(f[j-1]-f[j+1])^2
+IS2(f, j::Int) = 13/12*(f[j]-2f[j+1]+f[j+2])^2 + 1/4*(3f[j]-4f[j+1]+f[j+2])^2
 
 f = [f_0[end-1], f_0[end], f_0..., f_0[1], f_0[2]]
 
-t_e = 1
+t_e = 4.0
 
 CFL = 0.1
 Δt = CFL * hx
 
-for (i,_) in enumerate(0:Δt:t_e)
+function WENO5(f, p, t)
   f_hat = zeros(nx)
-  f_hat += C3_0 * map(i -> a3_01 * f[i - 2] + a3_02 * f[i - 1] + a3_03 * f[i], 3:nx+2)
-  f_hat += C3_1 * map(i -> a3_11 * f[i - 1] + a3_12 * f[i] + a3_13 * f[i + 1], 3:nx+2)
-  f_hat += C3_2 * map(i -> a3_21 * f[i] + a3_22 * f[i + 1] + a3_23 * f[i + 2], 3:nx+2)
 
-  
-  f[4:end-2] -= Δt * diff(f_hat) / hx
-  f[3] -= Δt * (f_hat[1] - f_hat[end]) / hx
+  IS_0 = (map(j -> IS0(f, j), 3:nx+2) .+ ϵ).^2
+  IS_1 = (map(j -> IS1(f, j), 3:nx+2) .+ ϵ).^2
+  IS_2 = (map(j -> IS2(f, j), 3:nx+2) .+ ϵ).^2
+
+  a0 = C3_0 ./ IS_0; a1 = C3_1 ./ IS_1; a2 = C3_2 ./ IS_2
+
+  a = a0 .+ a1 .+ a2
+
+  w0 = a0 ./ a; w1 = a1 ./ a; w2 = a2 ./ a;
+
+  f_hat .+= w0 .* map(i -> a3_01 * f[i - 2] + a3_02 * f[i - 1] + a3_03 * f[i], 3:nx+2)
+  f_hat .+= w1 .* map(i -> a3_11 * f[i - 1] + a3_12 * f[i] + a3_13 * f[i + 1], 3:nx+2)
+  f_hat .+= w2 .* map(i -> a3_21 * f[i] + a3_22 * f[i + 1] + a3_23 * f[i + 2], 3:nx+2)
 
   f[1] = f[end-3]
   f[2] = f[end-2]
@@ -61,15 +69,12 @@ for (i,_) in enumerate(0:Δt:t_e)
   f[end-1] = f[3]
   f[end] = f[4]
 
-  if i % 100 == 0
-    display(plot(xs, f[3:end-2]))
-  end
+  return -[0, 0, (f_hat[1] - f_hat[end]) / hx, diff(f_hat) / hx..., 0, 0]
 end
 
-for (i,_) in enumerate(0:Δt:t_e)
+function basic_upwind(f, p, t)
+
   f_hat = map(i -> 1/2 * (f[i] + f[i - 1]) , 3:nx+3)
-  
-  f[3:end-2] -= Δt * diff(f_hat) / hx
 
   f[1] = f[end-3]
   f[2] = f[end-2]
@@ -77,7 +82,12 @@ for (i,_) in enumerate(0:Δt:t_e)
   f[end-1] = f[3]
   f[end] = f[4]
 
-  if i % 100 == 0
-    display(plot(xs, f[3:end-2]))
-  end
+  return -[0, 0, diff(f_hat) / hx..., 0, 0]
+end
+
+prob = ODEProblem(WENO5, f, (0.0, t_e))
+soln = solve(prob, Tsit5())
+
+for t in range(0, t_e, length = 100)
+  display(plot(xs, soln(t)[3:end-2]))
 end
