b04959aad88ef10e110aca5b0b96a0cc34353c7f
GeorgeR227 <grauta@ufl.edu>
2026-02-18 17:28:31 -0500

    Improved WENO code, WENO 9, Burger's equation, shear advection test

--- test/CubicalTests/High-Res 2D.jl
+++ test/CubicalTests/High-Res 2D.jl
@@ -26,6 +26,7 @@ save("imgs/DualAdv.png", fig)
 # Rightwards motion
 V = init_tensor(Val(1), s)
 xedges(V) .= edge_len(s, 1, 1, 1)
+yedges(V) .= -edge_len(s, 1, 1, 1)
 
 bound_res = init_tensor(Val(2), s);
 
@@ -42,28 +43,43 @@ v_0 = init_tensor(Val(2), s)
 hodge_star!(v_0, Val(2), s, tensorfy_d(Val(0), s, u_0); inv = true)
 v = deepcopy(v_0)
 
-# TODO: This is only for uniform, rightward motion without boundary conditions
-function interior_product_quick(::Val{1}, s::EmbeddedCubicalComplex2D, X, f)
-  Xx = xedges(X)
-  Xy = yedges(X)
+function WENO_left_upwind(k::Int, s::EmbeddedCubicalComplex2D, f, i::Int, j::Int)
+  weno = UniformWENO{k, Float64}()
 
-  fx = d_xedges(f)
-  fy = d_yedges(f)
+  r = polyorder(weno)
+  1 + r <= i <= nx(s) - r || return 0
 
-  res = init_tensor_d(Val(0), s)
-
-  h = edge_len(s, 1, 1, 1)
-
-  for i in 2:nxquads(s)
-    for j in 1:nyquads(s)
-      res[i, j] = 0.5 * (Xx[i, j+1] + Xx[i, j]) * (3/8 * fx[i + 1, j] + 6/8 * fx[i,j] - 1/8 * fx[i-1,j]) / h^2
-    end
-  end
-
-  return res
+  return WENO(weno, RectStencil{k, Float64}(view(f, i-r:i+r, j)...))
 end
 
-function interior_product_upwind(::Val{1}, s::EmbeddedCubicalComplex2D, X, f)
+function WENO_right_upwind(k::Int, s::EmbeddedCubicalComplex2D, f, i::Int, j::Int)
+  weno = UniformWENO{k, Float64}()
+
+  r = polyorder(weno)
+  r <= i <= nx(s) - r - 1 || return 0
+
+  return WENO(weno, RectStencil{k, Float64}(view(f, i+r+1:-1:i-r+1, j)...))
+end
+
+function WENO_up_upwind(k::Int, s::EmbeddedCubicalComplex2D, f, i::Int, j::Int)
+  weno = UniformWENO{k, Float64}()
+
+  r = polyorder(weno)
+  1 + r <= j <= ny(s) - r || return 0
+
+  return WENO(weno, RectStencil{k, Float64}(view(f, i, j-r:j+r)...))
+end
+
+function WENO_down_upwind(k::Int, s::EmbeddedCubicalComplex2D, f, i::Int, j::Int)
+  weno = UniformWENO{k, Float64}()
+
+  r = polyorder(weno)
+  r <= j <= ny(s) - r - 1 || return 0
+
+  return WENO(weno, RectStencil{k, Float64}(view(f, i, j+r+1:-1:j-r+1)...))
+end
+
+function interior_product(::Val{1}, k::Int, s::EmbeddedCubicalComplex2D, X, f)
   Xx = xedges(X)
   Xy = yedges(X)
 
@@ -76,121 +92,18 @@ function interior_product_upwind(::Val{1}, s::EmbeddedCubicalComplex2D, X, f)
 
   for i in 1:nxquads(s)
     for j in 1:nyquads(s)
-      res[i, j] = 0.5 * (Xx[i, j+1] + Xx[i, j]) * fx[i, j] / h^2
-    end
-  end
-
-  return res
-end
-
-# TODO: Implement improved WENO-Z
-function WENO5_left_upwind(s::EmbeddedCubicalComplex2D, f, i::Int, j::Int)
-  # Optimal coefficients for combined stencil, 5th order
-  # Cr_k, r is order, k is stencil shift
-  C3_0 = 1/10
-  C3_1 = 6/10
-  C3_2 = 3/10
-
-  ϵ = 1e-40
-
-  # Coefficients for individual stencils, 3rd order
-  # ar_kl, r is order, j is stencil shift, l is term
-  a3_01 = 1/3
-  a3_02 = -7/6
-  a3_03 = 11/6
-
-  a3_11 = -1/6
-  a3_12 = 5/6
-  a3_13 = 1/3
-
-  a3_21 = 1/3
-  a3_22 = 5/6
-  a3_23 = -1/6
-
-  IS0(f, i::Int, j::Int) = 13/12*(f[i-2, j]-2f[i-1, j]+f[i, j])^2 + 1/4*(f[i-2, j]-4f[i-1, j]+3f[i, j])^2
-  IS1(f, i::Int, j::Int) = 13/12*(f[i-1, j]-2f[i, j]+f[i+1, j])^2 + 1/4*(f[i-1, j]-f[i+1, j])^2
-  IS2(f, i::Int, j::Int) = 13/12*(f[i, j]-2f[i+1, j]+f[i+2, j])^2 + 1/4*(3f[i, j]-4f[i+1, j]+f[i+2, j])^2
-
-
-  IS_0 = IS0(f, i, j); IS_1 = IS1(f, i, j); IS_2 = IS2(f, i, j);
-
-  tau_5 = abs(IS_0 - IS_2)
-
-  a0 = C3_0 * (1 + tau_5 / (IS_0 + ϵ)); a1 = C3_1 * (1 + tau_5 / (IS_1 + ϵ)); a2 = C3_2 * (1 + tau_5 / (IS_2 + ϵ));
-
-  a = a0 + a1 + a2
-
-  w0 = a0 / a; w1 = a1 / a; w2 = a2 / a;
-
-  flux_0 = w0 * (a3_01 * f[i - 2, j] + a3_02 * f[i - 1, j] + a3_03 * f[i, j])
-  flux_1 = w1 * (a3_11 * f[i - 1, j] + a3_12 * f[i, j] + a3_13 * f[i + 1, j])
-  flux_2 = w2 * (a3_21 * f[i, j] + a3_22 * f[i + 1, j] + a3_23 * f[i + 2, j])
-
-  return flux_0 + flux_1 + flux_2
-end
-
-function WENO5_right_upwind(s::EmbeddedCubicalComplex2D, f, i::Int, j::Int)
-  # Optimal coefficients for combined stencil, 5th order
-  # Cr_k, r is order, k is stencil shift
-  C3_0 = 1/10
-  C3_1 = 6/10
-  C3_2 = 3/10
-
-  ϵ = 1e-6
-
-  # Coefficients for individual stencils, 3rd order
-  # ar_kl, r is order, j is stencil shift, l is term
-  a3_01 = 1/3
-  a3_02 = -7/6
-  a3_03 = 11/6
-
-  a3_11 = -1/6
-  a3_12 = 5/6
-  a3_13 = 1/3
-
-  a3_21 = 1/3
-  a3_22 = 5/6
-  a3_23 = -1/6
-
-  IS0(f, i::Int, j::Int) = 13/12*(f[i+3, j]-2f[i+2, j]+f[i+1, j])^2 + 1/4*(f[i+3, j]-4f[i+2, j]+3f[i+1, j])^2
-  IS1(f, i::Int, j::Int) = 13/12*(f[i+2, j]-2f[i+1, j]+f[i, j])^2 + 1/4*(f[i+2, j]-f[i, j])^2
-  IS2(f, i::Int, j::Int) = 13/12*(f[i+1, j]-2f[i, j]+f[i-1, j])^2 + 1/4*(3f[i+1, j]-4f[i, j]+f[i-1, j])^2
-
-  IS_0 = IS0(f, i, j); IS_1 = IS1(f, i, j); IS_2 = IS2(f, i, j);
-
-  tau_5 = abs(IS_0 - IS_2)
-
-  a0 = C3_0 * (1 + tau_5 / (IS_0 + ϵ)); a1 = C3_1 * (1 + tau_5 / (IS_1 + ϵ)); a2 = C3_2 * (1 + tau_5 / (IS_2 + ϵ));
-
-  a = a0 + a1 + a2
-
-  w0 = a0 / a; w1 = a1 / a; w2 = a2 / a;
-
-  flux_0 = w0 * (a3_01 * f[i + 3, j] + a3_02 * f[i + 2, j] + a3_03 * f[i + 1, j])
-  flux_1 = w1 * (a3_11 * f[i + 2, j] + a3_12 * f[i + 1, j] + a3_13 * f[i, j])
-  flux_2 = w2 * (a3_21 * f[i + 1, j] + a3_22 * f[i, j] + a3_23 * f[i - 1, j])
-
-  return flux_0 + flux_1 + flux_2
-end
-
-function interior_product_WENO5(::Val{1}, s::EmbeddedCubicalComplex2D, X, f)
-  Xx = xedges(X)
-  Xy = yedges(X)
-
-  fx = d_xedges(f)
-  fy = d_yedges(f)
-
-  res = init_tensor_d(Val(0), s)
-
-  h = edge_len(s, 1, 1, 1)
-
-  for i in 3:nxquads(s) - 2
-    for j in 1:nyquads(s)
       interp_Xx = 0.5 * (Xx[i, j+1] + Xx[i, j])
       if interp_Xx >= 0
-        res[i, j] = interp_Xx * WENO5_left_upwind(s, fx, i, j) / h^2
+        res[i, j] += interp_Xx * WENO_left_upwind(k, s, fx, i, j) / h^2
       else
-        res[i, j] = interp_Xx * WENO5_right_upwind(s, fx, i, j) / h^2
+        res[i, j] += interp_Xx * WENO_right_upwind(k, s, fx, i, j) / h^2
+      end
+
+      interp_Xy = 0.5 * (Xy[i+1, j] + Xy[i, j])
+      if interp_Xy <= 0
+        res[i, j] += interp_Xy * WENO_up_upwind(k, s, fy, i, j) / h^2
+      else
+        res[i, j] += interp_Xy * WENO_down_upwind(k, s, fy, i, j) / h^2
       end
     end
   end
@@ -198,72 +111,24 @@ function interior_product_WENO5(::Val{1}, s::EmbeddedCubicalComplex2D, X, f)
   return res
 end
 
-t_e = 3.0
+t_e = 6.0
 
 save_res(arr) = detensorfy_d(Val(0), s, deepcopy(hodge_star!(final_res, Val(2), s, arr)))
 
-function upwind_adv(du, u, p, t)
-  boundary_quad_map!(bound_res, s, u, (1, 0))
-
-  hodge_star!(hdg2_res, Val(2), s, bound_res)
-  dual_derivative!(dd0_res, Val(0), s, hdg2_res)
-
-  iXv_res = interior_product_upwind(Val(1), s, V, dd0_res)
-
-  hodge_star!(invhdg2_res, Val(2), s, iXv_res, inv = true)
-
-  du .= -invhdg2_res
-end
-
-function QUICK_adv(du, u, p, t)
-  boundary_quad_map!(bound_res, s, u, (1, 0))
-
-  hodge_star!(hdg2_res, Val(2), s, bound_res)
-  dual_derivative!(dd0_res, Val(0), s, hdg2_res)
-
-  iXv_res = interior_product_quick(Val(1), s, V, dd0_res)
-
-  hodge_star!(invhdg2_res, Val(2), s, iXv_res, inv = true)
-
-  du .= -invhdg2_res
-end
+n = 9
 
 function WENO5_adv(du, u, p, t)
-
-  c = 1
-  if t >= 1.5
-    c = -1
-  end
-
-  boundary_quad_map!(bound_res, s, u, (1, 0))
+  boundary_quad_map!(bound_res, s, u, (n-1, n-1))
 
   hodge_star!(hdg2_res, Val(2), s, bound_res)
   dual_derivative!(dd0_res, Val(0), s, hdg2_res)
 
-  iXv_res = interior_product_WENO5(Val(1), s, c .* V, dd0_res)
+  iXv_res = interior_product(Val(1), n, s, V, dd0_res)
 
   hodge_star!(invhdg2_res, Val(2), s, iXv_res, inv = true)
 
   du .= -invhdg2_res
 end
-
-prob = ODEProblem(upwind_adv, v_0, (0.0, t_e))
-soln = solve(prob, Tsit5())
-
-fig = Figure();
-ax = CairoMakie.Axis(fig[1,1])
-mesh!(ax, sd; color = save_res(soln.u[2]))
-Colorbar(fig[1,2])
-save("imgs/UpwindAdvEnd.png", fig)
-
-prob = ODEProblem(QUICK_adv, v_0, (0.0, t_e))
-soln = solve(prob, Tsit5())
-
-fig = Figure();
-ax = CairoMakie.Axis(fig[1,1])
-mesh!(ax, sd; color = save_res(soln.u[end]))
-Colorbar(fig[1,2])
-save("imgs/QUICKDualAdvEnd.png", fig)
 
 prob = ODEProblem(WENO5_adv, v_0, (0.0, t_e))
 soln = solve(prob, Tsit5())
@@ -272,7 +137,7 @@ fig = Figure();
 ax = CairoMakie.Axis(fig[1,1])
 mesh!(ax, sd; color = save_res(soln.u[end]))
 Colorbar(fig[1,2])
-save("imgs/WENO5DualAdvEnd.png", fig)
+save("imgs/WENO$(n)DualAdvEnd.png", fig)
 
 function create_gif(solution, s::HasCubicalComplex, file_name, length)
   frames = range(0, t_e, length = length)
@@ -287,4 +152,4 @@ function create_gif(solution, s::HasCubicalComplex, file_name, length)
   end
 end
 
-create_gif(soln, sd, "imgs/WENO5_DualAdv.mp4", 100)
+create_gif(soln, sd, "imgs/WENO$(n)_DualAdv.mp4", 100)
--- test/CubicalTests/High-Res.jl
+++ test/CubicalTests/High-Res.jl
@@ -1,16 +1,20 @@
 using CairoMakie
 using Distributions
 using SparseArrays
+using StaticArrays
+using OrdinaryDiffEqTsit5
 
-nx = 101
-lx = 1.0
+include("../../src/CubicalCode/WENO.jl")
+
+nx = 251
+lx = 10.0
 
 h = lx / (nx - 1)
 
 points = range(0, lx; length = nx)
 dual_points = range(h / 2, lx - h / 2; length = nx - 1)
 
-c = 0.1
+c = h
 
 # ̇v = c∂v/∂x
 
@@ -44,51 +48,75 @@ function dual_derivative()
   return sparse(I, J, V)
 end
 
-
 d0 = dual_derivative()
 
-# This doesn't properly deal with boundary conditions
-# Between a one form and a dual one form
-function upwinding_interior(X, dω)
+function WENO_left_upwind(k::Int, f, i::Int)
+  weno = UniformWENO{k, Float64}()
+
+  r = polyorder(weno)
+  1 + r <= i <= nx - r - 1 || return 0
+
+  return WENO(weno, RectStencil{k, Float64}(view(f, i-r:i+r)...))
+end
+
+function interior_product(::Val{1}, k::Int, f)
   res = zeros(nx - 1)
-  res[end] = X[end] * dω[1] / h^2
-  for i in 1:nx-2
-    res[i] = X[i] * dω[i+1] / h^2
+
+  for i in 1:nx-1
+    res[i] = WENO_left_upwind(k, f, i) / h^2
   end
+
   return res
 end
 
-# We're basically just implementing the wedge product since the 1 / h^2 is basically the two Hodges
-function upwinding_interior_quick(X, dω)
-  res = zeros(nx - 1)
-  res[end] = X[end] * (3/8 * dω[end] + 6/8 * dω[1] - 1/8 * dω[2]) / h^2
-  res[end - 1] = X[end - 1] * (3/8 * dω[end-1] + 6/8 * dω[end] - 1/8 * dω[1]) / h^2
-  for i in 1:nx-1-2
-    res[i] = X[i] * (3/8 * dω[i] + 6/8 * dω[i+1] - 1/8 * dω[i+2]) / h^2
+# # This doesn't properly deal with boundary conditions
+# # Between a one form and a dual one form
+# function upwinding_interior(X, dω)
+#   res = zeros(nx - 1)
+#   for i in 1:nx-2
+#     res[i] = X[i] * dω[i+1] / h^2
+#   end
+#   return res
+# end
+
+dist = Normal(lx / 2, 0.5)
+u_0 = [pdf(dist, x) for x in dual_points];
+
+t_e = 0.2
+
+fig = Figure();
+ax = CairoMakie.Axis(fig[1,1])
+lines!(ax, dual_points, u_0)
+save("imgs/BurgerInit.png", fig)
+
+weno_order = 9
+
+function burger(du, u, p, t)
+  du .= -interior_product(Val(1), weno_order, d0 * (u .^2 / 2))
+end
+
+prob = ODEProblem(burger, u_0, (0.0, t_e))
+soln = solve(prob, Tsit5())
+soln.retcode
+
+fig = Figure();
+ax = CairoMakie.Axis(fig[1,1])
+lines!(ax, dual_points, soln.u[50])
+save("imgs/BurgerFinal.png", fig)
+
+function create_gif(solution, file_name, length = 100)
+  time = Observable(0.0)
+
+  y = @lift(soln($time))
+
+  fig = lines(dual_points, y, color = :blue, linewidth = 4,
+      axis = (title = @lift("t = $(round($time, digits = 3))"),))
+
+  timestamps = range(0, t_e, length = length)
+
+  record(fig, file_name, timestamps) do t
+      time[] = t
   end
-  return res
 end
 
-X = [c for i in 1:nx-1];
-
-dist = Normal(lx / 2, 0.1)
-ω_0 = [pdf(dist, x) for x in dual_points];
-
-Δt = 0.001
-t_e = 20 * Δt
-
-ω = deepcopy(ω_0)
-
-fig = Figure();
-ax = CairoMakie.Axis(fig[1,1])
-plot!(ax, dual_points, ω_0)
-fig
-
-for _ in 0:Δt:t_e
-  ω .= ω .+ Δt * upwinding_interior_quick(X, d0 * ω)
-end
-
-fig = Figure();
-ax = CairoMakie.Axis(fig[1,1])
-plot!(ax, dual_points, ω)
-fig
+create_gif(soln, "imgs/WENO$(weno_order)_Burger.mp4")
