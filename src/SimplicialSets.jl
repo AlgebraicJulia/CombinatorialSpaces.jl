df2c4fa3b3f28ee209e2e53e23966ba8f87af805
Luke Morris <lukelukemorrismorris@gmail.com>
2026-03-11 21:14:16 -0400

    Namespace collect with Base

--- src/SimplicialSets.jl
+++ src/SimplicialSets.jl
@@ -1067,7 +1067,7 @@ function boundary_inds(::Type{Val{0}}, s::HasDeltaSet2D)
 end
 
 function boundary_inds(::Type{Val{1}}, s::HasDeltaSet2D)
-  collect(findall(x -> x != 0, boundary(Val{2},s) * fill(1,ntriangles(s))))
+  Base.collect(findall(x -> x != 0, boundary(Val{2},s) * fill(1,ntriangles(s))))
 end
 
 function boundary_inds(::Type{Val{2}}, s::HasDeltaSet2D)
