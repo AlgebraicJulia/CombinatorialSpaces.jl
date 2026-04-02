31a3471e1209faee9082aad62a7cca026471b37d
GeorgeR227 <grauta@ufl.edu>
2026-04-02 15:10:32 -0400

    Added attempt at compressible cavity flow

--- test/CubicalTests/Low-Mach Cavity Flow.jl
+++ test/CubicalTests/Low-Mach Cavity Flow.jl
@@ -7,33 +7,12 @@ include("../../src/CubicalCode/UniformMesh.jl")
 include("../../src/CubicalCode/UniformMatrixDEC.jl")
 include("../../src/CubicalCode/UniformPlotting.jl")
 
-lx_ = ly_ = 2π;
-nx_ = ny_ = 81
-s = UniformCubicalComplex2D(nx_, ny_, lx_, ly_; halo_x = 5, halo_y = 5)
-
-function form_taylor_vortices(s::AbstractCubicalComplex2D, G::Real, a::Real, centers)
-  u = zeros(nv(s))
-
-  function taylor_vortex(pnt::Point3d, cntr::Point3d)
-    r = norm(pnt .- cntr)
-    (G / a) * (2 - (r / a)^2) * exp(0.5 * (1 - (r / a)^2))
-  end
-
-  for center in centers
-    u += map(p -> taylor_vortex(p, center), points(s))
-  end
-
-  return u
-end
-
-left_cnt = Point3d(lx_ / 2 - 0.4, ly_ / 2, 0.0); right_cnt = Point3d(lx_ / 2 + 0.4, ly_ / 2, 0.0)
-ω = form_taylor_vortices(s, 1.0, 0.3, [left_cnt, right_cnt])
-
-save("imgs/LMNS/Vortices.png", plot_zeroform(s, ω))
+lx_ = ly_ = 1.0;
+nx_ = ny_ = 129
+s = UniformCubicalComplex2D(nx_, ny_, lx_, ly_)
 
 # DEC Operators
 Δ0 = laplacian(Val(0), s);
-dΔ1 = dual_laplacian(Val(1), s);
 
 d0 = exterior_derivative(Val(0), s);
 d1 = exterior_derivative(Val(1), s);
@@ -43,6 +22,11 @@ dual_d1 = dual_derivative(Val(1), s);
 
 # TODO: Use this if we don't want periodic boundaries
 d_beta = 0.5 * abs.(dual_d1) * spdiagm(dual_d0 * ones(nquads(s)));
+
+boundary_idxs = findall(x -> x != 0, dual_d0 * ones(nquads(s)));
+top = top_edges(s);
+
+dual_d0[boundary_idxs, :] .= 0.0 # Enforce no-flux boundary condition on density
 
 hdg_1 = hodge_star(Val(1), s);
 hdg_2 = hodge_star(Val(2), s);
@@ -54,32 +38,35 @@ inv_hdg_2 = inv_hodge_star(Val(2), s);
 δ1 = codifferential(Val(1), s);
 dual_δ1 = dual_codifferential(Val(1), s);
 
-# Solve for initial conditions
-ψ = Δ0 \ ω;
-ψ .= ψ .- minimum(ψ);
+# Enforce closure of dual cells and no symmetric boundary conditions on velocity
+dΔ1 = hdg_1 * d0 * inv_hdg_0 * dual_d1 + dual_d0 * hdg_2 * d1 * inv_hdg_1
+dΔ1_V = hdg_1 * d0 * inv_hdg_0 * d_beta
 
-u_star_0 = d0 * ψ;
-ω_test = δ1 * u_star_0;
-
-save("imgs/LMNS/RoundTrip_Vortices.png", plot_zeroform(s, ω_test))
+u_star_0 = zeros(ne(s))
 
 function momentum_continuity(U_star, rho_star, p)
   U = hdg_1 * U_star
   rho = hdg_2 * rho_star
   κ, μ = p
 
+  U[boundary_idxs] .= 0.0 # Enforce no-flux boundary condition on momentum
+
   # Compute velocity u from momentum U and density rho
   u = wedge_product_dd(Val(0), Val(1), s, 1 ./ rho, U)
 
   # Interpolate u to primal edges
   v = interpolate_dp(Val(1), s, u)
+  V = interpolate_dp(Val(1), s, U)
+
+  v[boundary_idxs] .= 0.0 # Enforce no-slip boundary condition on velocity
+  v[top] .= edge_len(s, X_ALIGN) # Lid velocity
 
   # U ∧ δu
   div_term = wedge_product_dd(Val(1), Val(0), s, U, dual_δ1 * u)
 
   # L(u, U)
   L_term = dual_d0 * hdg_2 * wedge_product(Val(1), Val(1), s, v, inv_hdg_1 * U) +
-    hdg_1 * wedge_product(Val(1), Val(0), s, v, inv_hdg_0 * dual_d1 * U)
+    hdg_1 * wedge_product(Val(1), Val(0), s, v, inv_hdg_0 * (dual_d1 * U + d_beta * V))
 
   # 1/2 * ρ * d||u||^2
   diff_norm_u = dual_d0 * hdg_2 * wedge_product(Val(1), Val(1), s, v, inv_hdg_1 * u)
@@ -89,14 +76,7 @@ function momentum_continuity(U_star, rho_star, p)
   diff_p = dual_d0 * (κ * rho)
 
   # μΔu
-  lap_term = μ * dΔ1 * u
-
-  # println("Extrema of terms: ")
-  # println("  Div: $(minimum(div_term)) to $(maximum(div_term))")
-  # println("  L: $(minimum(L_term)) to $(maximum(L_term))")
-  # println("  Energy: $(minimum(energy)) to $(maximum(energy))")
-  # println("  Diff P: $(minimum(diff_p)) to $(maximum(diff_p))")
-  # println("  Laplacian: $(minimum(lap_term)) to $(maximum(lap_term))")
+  lap_term = μ * (dΔ1 * u + dΔ1_V * v)
 
   return -inv_hdg_1 * (-div_term - L_term + energy - diff_p + lap_term)
 end
@@ -119,20 +99,9 @@ function run_compressible_ns(U_star_0, rho_star_0, tₑ, Δt, p; saveat=500)
   m₀ = sum(interior(Val(2), rho_star_0, s))
 
   for step in 1:steps
-
-    set_periodic!(U_star, Val(1), s, ALL)
-    set_periodic!(rho_star, Val(2), s, ALL)
-
-    # Update momentum to half step
-    # Use that to update density to half step (and also full step)
-    # Use both to update momentum to full step
-
     U_star_half .= U_star .+ 0.5 * Δt * momentum_continuity(U_star, rho_star, p)
-    set_periodic!(U_star_half, Val(1), s, ALL)
 
     rho_star_full .= rho_star + Δt * d1 * U_star_half # Mass changes by momentum flux
-    set_periodic!(rho_star_full, Val(2), s, ALL)
-
     rho_star_half .= 0.5 .* (rho_star .+ rho_star_full)
 
     U_star .= U_star .+ Δt * momentum_continuity(U_star_half, rho_star_half, p)
@@ -164,31 +133,28 @@ function run_compressible_ns(U_star_0, rho_star_0, tₑ, Δt, p; saveat=500)
   return Us, rhos
 end
 
-Re = Inf
+Re = 10
 κ = 280 * 300 # R (Dry gas constant) * T = 300K, from P = ρRVT
 p = (κ, 1 / Re) # κ, μ
 rho_star_0 = inv_hdg_2 * ones(nquads(s))
 U_star_0 = deepcopy(u_star_0)
 
-te = 1;
-Δt = 1e-4;
+Us, rhos = run_compressible_ns(U_star_0, rho_star_0, 1e-2, 1e-5, p; saveat=10);
 
-Us, rhos = run_compressible_ns(U_star_0, rho_star_0, te, Δt, p; saveat=100);
-
-time = 5; # length(Us)
+time = length(Us)
 ω = inv_hdg_0 * dual_d1 * Us[time];
 
 fig = plot_zeroform(s, ω)
-save("imgs/LMNS/FinalVortices.png", fig)
+save("imgs/LMNS_Cav/FinalVortices.png", fig)
 
 fig = plot_twoform(s, rhos[time])
-save("imgs/LMNS/FinalDensity.png", fig)
+save("imgs/LMNS_Cav/FinalDensity.png", fig)
 
 function create_mp4(filename, Us, rhos; frames = length(Us), framerate = 15)
 
   fig = Figure(size=(1000, 1000))
   ax1 = Axis(fig[1, 1], title = "Density Field (p)")
-  ax2 = Axis(fig[2, 1], title = "Vorticity Field")
+  ax2 = Axis(fig[2, 1], title = "Velocity Magnitude")
 
   dps = interior(Val(2), dual_points(s), s)
   xs = map(a -> a[1], dps)
@@ -197,10 +163,15 @@ function create_mp4(filename, Us, rhos; frames = length(Us), framerate = 15)
   step = Observable(1)
 
   rho = @lift(interior(Val(2), rhos[$step], s))
-  rot = @lift(interior(Val(0), inv_hdg_0 * dual_d1 * Us[$step], s))
+  vel_mag = @lift(begin
+    u_step = wedge_product_dd(Val(0), Val(1), s, 1 ./ rhos[$step], Us[$step])
+    X_step = zeros(nquads(s)); Y_step = zeros(nquads(s))
+    sharp_dd!(X_step, Y_step, s, u_step)
+    interior(Val(2), sqrt.(X_step.^2 .+ Y_step.^2), s)
+  end)
 
   scatter1 = heatmap!(ax1, xs, ys, rho, colormap=Reverse(:oslo))
-  scatter2 = mesh!(ax2, s, color = rot, colormap=:viridis, colorrange=(-5, 12))
+  scatter2 = heatmap!(ax2, xs, ys, vel_mag, colormap=:viridis)
 
   Colorbar(fig[1, 2], scatter1, tickformat = ticks -> [@sprintf("%.1f", t) for t in ticks])
   Colorbar(fig[2, 2], scatter2)
@@ -213,4 +184,4 @@ function create_mp4(filename, Us, rhos; frames = length(Us), framerate = 15)
   end
 end
 
-create_mp4("imgs/LMNS/simulation.mp4", Us, rhos; framerate = 15)
+create_mp4("imgs/LMNS_Cav/simulation.mp4", Us, rhos; framerate = 15)
