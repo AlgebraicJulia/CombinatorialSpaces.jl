e26e97472574257d161b878e02a440207015b6ca
GeorgeR227 <grauta@ufl.edu>
2026-06-18 14:29:43 +0000

    Added signaling to IO

--- src/CubicalCode/UniformIO.jl
+++ src/CubicalCode/UniformIO.jl
@@ -13,18 +13,69 @@ struct Quad <: AbstractMeshType end
 struct Boid <: AbstractMeshType end
 
 # This logically represents a piece of data (density, temperature, velocity, etc.)
-struct Datum{M <: AbstractMeshType, N}
-    name      :: String
-    groupname :: String
-    entrytype :: DataType
+struct Datum{M<:AbstractMeshType,N}
+    name::String
+    groupname::String
+    entrytype::DataType
 end
 
-Datum{M, N}() where {M <: AbstractMeshType, N} = Datum{M, N}("", "", Float64)
+Datum{M,N}() where {M<:AbstractMeshType,N} = Datum{M,N}("", "", Float64)
 
-Datum{Boid, 2}(args...) = 
-    error("Datum{Boid, 2} is invalid: Boid is a 3-cell and only exists in 3D. " * 
-    "Did you mean Datum{Quad, 2} or Datum{Boid, 3}?")
+function Datum{Boid,2}(args...)
+    return error(
+        "Datum{Boid, 2} is invalid: Boid is a 3-cell and only exists in 3D. " *
+        "Did you mean Datum{Quad, 2} or Datum{Boid, 3}?",
+    )
+end
 
+nfamilies(::Datum{Vert,N}) where {N} = 1
+nfamilies(::Datum{Boid,N}) where {N} = 1
+nfamilies(::Datum{Quad,2}) = 1
+nfamilies(::Datum{Quad,3}) = 3
+nfamilies(::Datum{Edge,N}) where {N} = N
+
+function datum_dset_names(datum::Datum)
+    n = nfamilies(datum)
+    return n == 1 ? [datum.name] : [datum.name * "_$i" for i in 1:n]
+end
+
+datum_dset_paths(datum::Datum) = (datum.groupname * "/") .* datum_dset_names(datum)
+open_dsets(h5loc, datum::Datum) = [h5loc[path] for path in datum_dset_paths(datum)]
+
+datum_dims(::Datum{Vert,2}, m::AbstractCubicalComplex2D) = [(nx(m), ny(m))]
+datum_dims(::Datum{Vert,3}, m::AbstractCubicalComplex3D) = [(nx(m), ny(m), nz(m))]
+
+datum_dims(::Datum{Edge,2}, m::AbstractCubicalComplex2D) = [
+    (nxe(m), ny(m)),   # X family
+    (nx(m), nye(m)),
+]  # Y family
+function datum_dims(::Datum{Edge,3}, m::AbstractCubicalComplex3D)
+    return [
+        (nxe(m), ny(m), nz(m)),   # X family
+        (nx(m), nye(m), nz(m)),   # Y family
+        (nx(m), ny(m), nze(m)),
+    ]  # Z family
+end  # Z family
+
+datum_dims(::Datum{Quad,2}, m::AbstractCubicalComplex2D) = [(nxq(m), nyq(m))]
+function datum_dims(::Datum{Quad,3}, m::AbstractCubicalComplex3D)
+    return [
+        (nxq(m), nyq(m), nz(m)),   # XY family
+        (nxq(m), ny(m), nzq(m)),  # XZ family
+        (nx(m), nyq(m), nzq(m)),
+    ]  # YZ family
+end  # YZ family
+
+datum_dims(::Datum{Boid,2}, m::AbstractCubicalComplex2D) = [(nxq(m), nyq(m))]
+datum_dims(::Datum{Boid,3}, m::AbstractCubicalComplex3D) = [(nxb(m), nyb(m), nzb(m))]
+
+function tile_buffer(datum::Datum, m::AbstractCubicalComplex)
+    return [Array{datum.entrytype}(undef, dims...) for dims in datum_dims(datum, m)]
+end
+
+function mesh_count(datum::Datum, m::AbstractCubicalComplex)
+    return sum(prod(dims) for dims in datum_dims(datum, m))
+end
 
 # This logically represnts a stream of data either meant to be read out or saved
 struct DataStream
@@ -36,7 +87,27 @@ end
 DataStream(datum::Datum) = DataStream("", "", [datum])
 DataStream(data::AbstractVector{Datum}) = DataStream("", "", data)
 
-# ^ The two above are totally user created
+abstract type AbstractMetaData end  # stub, filled later
+
+# ── Signalling ────────────────────────────────────────────────────────────────
+
+@enum SignalTag::Int32 begin
+    SIGNAL_WRITE = 0
+    SIGNAL_DONE = -1
+end
+
+function worker_to_output(tag::SignalTag, topo::MPITopology{<:WorkerCache})
+    worker_leader(topo) || return nothing
+    buf = Ref{Int32}(Int32(tag))
+    MPI.Send(buf, topo.intercomm; dest = 0)
+    return nothing
+end
+
+function output_from_worker(topo::MPITopology{<:OutputCache})
+    buf = Ref{Int32}(Int32(0))
+    MPI.Recv!(buf, topo.intercomm; source = 0)
+    return SignalTag(buf[])
+end
 
 mutable struct GathervCache{T}
     recv_buffer::Vector{T}
@@ -44,35 +115,34 @@ mutable struct GathervCache{T}
     displs::Vector{Cint}
 end
 
-function GathervCache(datum::Datum, cache::OutputCache{N}) where N
+function GathervCache(datum::Datum, cache::OutputCache{N}) where {N}
     T = datum.entrytype
 
     recv_counts = Cint[mesh_count(datum, wc.mesh) for wc in cache.worker_caches]
-    displs      = Cint[0; cumsum(recv_counts)[1:end-1]]
+    displs = Cint[0; cumsum(recv_counts)[1:(end - 1)]]
     recv_buffer = Vector{T}(undef, Int(sum(recv_counts)))
 
     return GathervCache{T}(recv_buffer, recv_counts, displs)
 end
 
 mutable struct DataHandler{N}
-    stream        :: DataStream
-    topo          :: MPITopology{OutputCache{N}}
-    gatherv_caches:: AbstractVector{GathervCache}
-    tile_buffers  :: AbstractVector{AbstractArray} # Temp holding for data from Gatherv
-    save_step     :: Int
+    stream::DataStream
+    topo::MPITopology{OutputCache{N}}
+    gatherv_caches::AbstractVector{GathervCache}
+    tile_buffers::AbstractVector{AbstractArray} # Temp holding for data from Gatherv
 end
 
-out_cache(h::DataHandler)      = h.topo.cache
-out_cart_comm(h::DataHandler)  = h.topo.cart_comm
-intercomm(h::DataHandler)  = h.topo.intercomm
-is_output(h::DataHandler)  = h.topo.is_output
-om_mesh(h::DataHandler)    = out_cache(h).om_mesh
+out_cache(h::DataHandler) = h.topo.cache
+out_cart_comm(h::DataHandler) = h.topo.cart_comm
+intercomm(h::DataHandler) = h.topo.intercomm
+is_output(h::DataHandler) = h.topo.is_output
+om_mesh(h::DataHandler) = out_cache(h).om_mesh
 om_gm_offsets(h::DataHandler) = out_cache(h).om_gm_offsets
 worker_caches(h::DataHandler) = out_cache(h).worker_caches
 lm_om_offsets(h::DataHandler) = out_cache(h).lm_om_offsets
 metadata(h::DataHandler) = h.save_step
 
-function DataHandler(stream::DataStream, topo::MPITopology{OutputCache{N}}) where N
+function DataHandler(stream::DataStream, topo::MPITopology{OutputCache{N}}) where {N}
     cache = topo.cache
 
     gatherv_caches = map(datum -> GathervCache(datum, cache), stream.data)
@@ -81,18 +151,18 @@ function DataHandler(stream::DataStream, topo::MPITopology{OutputCache{N}}) wher
     return DataHandler{N}(stream, topo, gatherv_caches, tile_buffers, 1)
 end
 
-function DataHandler(stream::DataStream, cache::OutputCache{N}) where N
+function DataHandler(stream::DataStream, cache::OutputCache{N}) where {N}
     topo = MPITopology(cache, true)
     return DataHandler(stream, topo)
 end
 
-function _build_gatherv_counts(datum::Datum, worker_caches::Vector{OutputWorkerCache{N}}) where N
+function _build_gatherv_counts(datum::Datum, worker_caches::Vector{OutputWorkerCache{N}}) where {N}
     recv_counts = Cint[mesh_count(datum, wc.mesh) for wc in worker_caches]
-    displs      = Cint[0; cumsum(recv_counts)[1:end-1]]
+    displs = Cint[0; cumsum(recv_counts)[1:(end - 1)]]
     return recv_counts, displs
 end
 
-function create_hdf5!(handler::DataHandler{N}, gm_dims::NTuple{N, Int}) where N
+function create_hdf5!(handler::DataHandler{N}, gm_dims::NTuple{N,Int}) where {N}
     gm = PseudoCubicalMesh(gm_dims...)
     h5open(handler.stream.filepath, "w", out_cart_comm(handler), MPI.Info()) do h5
         for datum in handler.stream.data
@@ -100,25 +170,29 @@ function create_hdf5!(handler::DataHandler{N}, gm_dims::NTuple{N, Int}) where N
                 create_group(h5, datum.groupname)
             end
             grp = h5[datum.groupname]
-
             for (name, spatial_dims) in zip(datum_dset_names(datum), datum_dims(datum, gm))
-                dims    = tuple(1, spatial_dims...)
+                dims = tuple(0, spatial_dims...)
                 maxdims = tuple(-1, spatial_dims...)
-                chunk   = tuple(1, spatial_dims...)
-
-                HDF5.create_dataset(grp, name, datum.entrytype,
-                    HDF5.dataspace(dims, max_dims=maxdims);
-                    chunk=chunk,
-                    dxpl_mpio=:collective)
+                chunk = tuple(1, spatial_dims...)
+                HDF5.create_dataset(
+                    grp,
+                    name,
+                    datum.entrytype,
+                    HDF5.dataspace(dims; max_dims = maxdims);
+                    chunk = chunk,
+                    dxpl_mpio = :collective,
+                )
             end
         end
     end
 end
 
-function write_output!(handler::DataHandler{N}) where N
+# Output-side
+function write_output!(handler::DataHandler{N}) where {N}
     gather!(handler)
 
-    for (datum, gcache, tbuf) in zip(handler.stream.data, handler.gatherv_caches, handler.tile_buffers)
+    for (datum, gcache, tbuf) in
+        zip(handler.stream.data, handler.gatherv_caches, handler.tile_buffers)
         scatter_to_tile!(datum, gcache, tbuf, handler)
     end
 
@@ -126,123 +200,103 @@ function write_output!(handler::DataHandler{N}) where N
         for (datum, datum_tbufs) in zip(handler.stream.data, handler.tile_buffers)
             dsets = open_dsets(h5, datum)
             for (dset, tbuf) in zip(dsets, datum_tbufs)
-                extent_dims, _ = HDF5.get_extent_dims(HDF5.dataspace(dset))
-                time_dim, spatial_dims... = extent_dims
-                if time_dim < handler.save_step
-                    HDF5.set_extent_dims(dset, tuple(handler.save_step, spatial_dims...))
-                end
-                write_tile!(dset, tbuf, handler)
+                time_dim, spatial_dims... = HDF5.get_extent_dims(HDF5.dataspace(dset))[1]
+                HDF5.set_extent_dims(dset, (time_dim + 1, spatial_dims...))
+                write_tile!(dset, tbuf, time_dim + 1, handler)
             end
         end
     end
 
-    handler.save_step += 1
+    return handler.save_step += 1
 end
 
-nfamilies(::Datum{Vert, N}) where N = 1
-nfamilies(::Datum{Boid, N}) where N = 1
-nfamilies(::Datum{Quad, 2})         = 1
-nfamilies(::Datum{Quad, 3})         = 3
-nfamilies(::Datum{Edge, N}) where N = N
-
-function datum_dset_names(datum::Datum)
-    n = nfamilies(datum)
-    return n == 1 ? [datum.name] :
-                    [datum.name * "_$i" for i in 1:n]
-end
-
-datum_dset_paths(datum::Datum) = (datum.groupname * "/") .* datum_dset_names(datum)
-open_dsets(h5loc, datum::Datum) = [h5loc[path] for path in datum_dset_paths(datum)]
-
-function write_output!(data_arrays::Vector{<:AbstractVector}, stream::DataStream, topo::MPITopology{WorkerCache{N}}) where N
+# Worker-side
+function send_output!(
+    data_arrays::Vector{<:AbstractVector},
+    stream::DataStream,
+    topo::MPITopology{<:WorkerCache},
+)
+    worker_to_output(SIGNAL_WRITE, topo)
     for (data, datum) in zip(data_arrays, stream.data)
         gather!(data, datum, topo)
     end
 end
 
-function gather!(handler::DataHandler{N}) where N
+function gather!(handler::DataHandler{N}) where {N}
     mpi_root = Ref{Cint}(MPI.API.MPI_ROOT[])
 
     for (datum, gcache) in zip(handler.stream.data, handler.gatherv_caches)
         GC.@preserve gcache begin
             MPI.API.MPI_Gatherv(
-                C_NULL, Cint(0), MPI.Datatype(datum.entrytype),
-                gcache.recv_buffer, gcache.recv_counts, gcache.displs, MPI.Datatype(datum.entrytype),
-                mpi_root[], handler.topo.intercomm
+                C_NULL,
+                Cint(0),
+                MPI.Datatype(datum.entrytype),
+                gcache.recv_buffer,
+                gcache.recv_counts,
+                gcache.displs,
+                MPI.Datatype(datum.entrytype),
+                mpi_root[],
+                handler.topo.intercomm,
             )
         end
     end
 end
 
-function gather!(data::AbstractVector{T}, datum::Datum{M, N}, topo::MPITopology{WorkerCache{N}}) where {T, M, N}
+function gather!(
+    data::AbstractVector{T},
+    datum::Datum{M,N},
+    topo::MPITopology{WorkerCache{N}},
+) where {T,M,N}
     GC.@preserve data begin
         MPI.API.MPI_Gatherv(
-            data, Cint(length(data)), MPI.Datatype(datum.entrytype),
-            C_NULL, C_NULL, C_NULL,   MPI.Datatype(datum.entrytype),
-            Cint(0), topo.intercomm
+            data,
+            Cint(length(data)),
+            MPI.Datatype(datum.entrytype),
+            C_NULL,
+            C_NULL,
+            C_NULL,
+            MPI.Datatype(datum.entrytype),
+            Cint(0),
+            topo.intercomm,
         )
     end
 end
 
-function scatter_to_tile!(datum::Datum, gcache::GathervCache, tbufs::Vector, handler::DataHandler{N}) where N
-    src_starts = [0; cumsum([mesh_count(datum, wc.mesh) for wc in worker_caches(handler)])[1:end-1]]
+function scatter_to_tile!(
+    datum::Datum,
+    gcache::GathervCache,
+    tbufs::Vector,
+    handler::DataHandler{N},
+) where {N}
+    src_starts =
+        [0; cumsum([mesh_count(datum, wc.mesh) for wc in worker_caches(handler)])[1:(end - 1)]]
 
-    for (wc, wc_offset, src_start) in zip(worker_caches(handler), lm_om_offsets(handler), src_starts)
+    for (wc, wc_offset, src_start) in
+        zip(worker_caches(handler), lm_om_offsets(handler), src_starts)
         for (tbuf, dims) in zip(tbufs, datum_dims(datum, wc.mesh))
             _scatter_worker_chunk!(tbuf, gcache.recv_buffer, wc_offset, src_start, dims)
         end
     end
 end
 
-function _scatter_worker_chunk!(tbuf::AbstractArray, recv_buffer::AbstractVector, wc_offset::NTuple{N, Int},
-                                 src_start::Int, dims::NTuple{N, Int}) where N
-    n      = prod(dims)
-    ranges = ntuple(i -> wc_offset[i]+1 : wc_offset[i]+dims[i], N)
-    tbuf[ranges...] .= reshape(recv_buffer[src_start+1 : src_start+n], dims)
+function _scatter_worker_chunk!(
+    tbuf::AbstractArray,
+    recv_buffer::AbstractVector,
+    wc_offset::NTuple{N,Int},
+    src_start::Int,
+    dims::NTuple{N,Int},
+) where {N}
+    n = prod(dims)
+    ranges = ntuple(i -> (wc_offset[i] + 1):(wc_offset[i] + dims[i]), N)
+    return tbuf[ranges...] .= reshape(recv_buffer[(src_start + 1):(src_start + n)], dims)
 end
 
-function write_tile!(dset, tbuf::Array, handler::DataHandler{N}) where N
+function write_tile!(dset, tbuf::Array, step::Int, handler::DataHandler{N}) where {N}
     ranges = _hyperslab_ranges(out_cache(handler), size(tbuf))
-    dset[handler.save_step, ranges...] = tbuf
-    return dset
+    return dset[step, ranges...] = tbuf
 end
 
-function _hyperslab_ranges(cache::OutputCache{N}, count::NTuple{N,Int}) where N
+function _hyperslab_ranges(cache::OutputCache{N}, count::NTuple{N,Int}) where {N}
     offset = cache.om_gm_offsets
-    return ntuple(j -> offset[j]+1 : offset[j]+count[j], N)
-end
-
-# ── datum_dims ────────────────────────────────────────────────────────────────
-# This returns a vector to handle aligned elements seperatly (edges in 2D/3D, or quads in 3D)
-
-datum_dims(::Datum{Vert, 2}, m::AbstractCubicalComplex2D) = [(nx(m), ny(m))]
-datum_dims(::Datum{Vert, 3}, m::AbstractCubicalComplex3D) = [(nx(m), ny(m), nz(m))]
-
-datum_dims(::Datum{Edge, 2}, m::AbstractCubicalComplex2D) =
-    [(nxe(m), ny(m)),   # X family
-     (nx(m),  nye(m))]  # Y family
-datum_dims(::Datum{Edge, 3}, m::AbstractCubicalComplex3D) =
-    [(nxe(m), ny(m),  nz(m)),   # X family
-     (nx(m),  nye(m), nz(m)),   # Y family
-     (nx(m),  ny(m),  nze(m))]  # Z family
-
-datum_dims(::Datum{Quad, 2}, m::AbstractCubicalComplex2D) = [(nxq(m), nyq(m))]
-datum_dims(::Datum{Quad, 3}, m::AbstractCubicalComplex3D) =
-    [(nxq(m), nyq(m), nz(m)),   # XY family
-     (nxq(m), ny(m),  nzq(m)),  # XZ family
-     (nx(m),  nyq(m), nzq(m))]  # YZ family
-
-datum_dims(::Datum{Boid, 2}, m::AbstractCubicalComplex2D) = [(nxq(m), nyq(m))]
-datum_dims(::Datum{Boid, 3}, m::AbstractCubicalComplex3D) = [(nxb(m), nyb(m), nzb(m))]
-
-# ── tile_buffer ───────────────────────────────────────────────────────────────
-
-function tile_buffer(datum::Datum, m::AbstractCubicalComplex)
-    [Array{datum.entrytype}(undef, dims...) for dims in datum_dims(datum, m)]
-end
-
-# ── mesh_count ────────────────────────────────────────────────────────────────
-
-function mesh_count(datum::Datum, m::AbstractCubicalComplex)
-    sum(prod(dims) for dims in datum_dims(datum, m))
+    return ntuple(j -> (offset[j] + 1):(offset[j] + count[j]), N)
 end
---
+++ test/CubicalTests/New_Heat_2D_MPI.jl
@@ -0,0 +1,267 @@
+# Heat_Dual2D_MPI.jl
+#
+# 2D heat equation on dual 0-forms (quads), MPI-parallel with HDF5 output.
+# Uses the new MPITopology worker/output split from UniformMPI.jl and the
+# DataHandler pipeline from UniformIO.jl.
+#
+# Physics pipeline (u = dual 0-form on quads):
+#   grad_u      = dual_derivative(Val(0), s, u)     quad → edge
+#   flux_dual   = -k * grad_u
+#   flux_primal = inv_hodge_star(Val(1), s, flux)   dual 1 → primal 1
+#   div_flux    = exterior_derivative(Val(1), s, ..) primal 1 → primal 2
+#   laplacian_u = hodge_star(Val(2), s, div_flux)   primal 2 → dual 0
+#
+# Periodicity handled by halo exchange before each RHS evaluation.
+
+using MPI
+using HDF5
+using OrdinaryDiffEqTsit5
+using Distributions
+using KernelAbstractions
+using DiffEqCallbacks
+using Printf
+using CairoMakie
+
+include("../../src/CubicalCode/UniformMesh.jl")
+include("../../src/CubicalCode/UniformMatrixDEC.jl")
+include("../../src/CubicalCode/UniformKernelDEC.jl")
+include("../../src/CubicalCode/UniformMPI.jl")
+include("../../src/CubicalCode/UniformIO.jl")
+
+# ── Global problem parameters ─────────────────────────────────────────────────
+
+const NXQ_GLOBAL = 79
+const NYQ_GLOBAL = 79
+const NX_GLOBAL = NXQ_GLOBAL + 1
+const NY_GLOBAL = NYQ_GLOBAL + 1
+const m_dims = (NX_GLOBAL, NY_GLOBAL)
+
+const LX = 5.0
+const LY = 5.0
+const K_DIFFUSION = 0.5
+const T_START = 0.0
+const T_END = 1.0
+const DT = 0.001
+const SAVEAT = 0.025
+const HALO = 5
+const PRINT_EVERY_N_STEPS = 250
+
+const OUTDIR = "output_heat2D_mpi"
+const OUTFILE = joinpath(OUTDIR, "heat2D.h5")
+const IMGDIR = "imgs/Heat2D_MPI"
+
+FT = Float64
+
+# ── DataStream ────────────────────────────────────────────────────────────────
+
+const datum = Datum{Quad,2}("snapshots", "fields", FT)
+const stream = DataStream("heat2D", OUTFILE, [datum])
+
+# ── Build topology ────────────────────────────────────────────────────────────
+# Worker/output split: workers run the PDE, outputs handle HDF5.
+
+const w_dims = (2, 2)
+const o_dims = (1, 1)
+
+MPI.Init()
+topo = MPITopology(m_dims, w_dims, o_dims; periods = (true, true))
+
+world_comm = topo.world_comm
+world_rank = MPI.Comm_rank(world_comm)
+cart_comm = topo.cart_comm
+cart_rank = MPI.Comm_rank(cart_comm)
+
+println("ENTERING FIRST BARRIER AS RANK $world_rank")
+MPI.Barrier(world_comm)
+println("PAST FIRST BARRIER AS RANK $world_rank")
+
+# ── Output branch ─────────────────────────────────────────────────────────────
+
+if output(topo)
+    handler = DataHandler(stream, topo)
+
+    output_leader(topo) && rm(OUTDIR; recursive = true, force = true)
+    MPI.Barrier(cart_comm)
+    output_leader(topo) && mkpath(OUTDIR)
+    MPI.Barrier(cart_comm)
+
+    create_hdf5!(handler, m_dims)
+    MPI.Barrier(cart_comm)
+
+    # Busy-wait loop: receive a signal from paired worker leader over intercomm.
+    while true
+        tag = output_from_worker(topo)
+        tag == SIGNAL_DONE && break
+        tag == SIGNAL_WRITE && write_output!(handler)
+    end
+    MPI.Barrier(cart_comm)
+
+    # ── Worker branch ─────────────────────────────────────────────────────────────
+
+else
+    cache = topo.cache
+    cart_coords = MPI.Cart_coords(cart_comm)
+    cart_dims, _, _ = MPI.Cart_get(cart_comm)
+
+    # ── Local mesh construction ───────────────────────────────────────────────
+
+    nxr_local = cache.lm_dims[1]
+    nyr_local = cache.lm_dims[2]
+    x_offset = cache.lm_gm_offsets[1]
+    y_offset = cache.lm_gm_offsets[2]
+
+    local_lx = FT(nxr_local - 1) * LX / NXQ_GLOBAL
+    local_ly = FT(nyr_local - 1) * LY / NYQ_GLOBAL
+    base_x_ = FT(x_offset) * LX / NXQ_GLOBAL
+    base_y_ = FT(y_offset) * LY / NYQ_GLOBAL
+
+    s = UniformCubicalComplex2D(
+        nxr_local,
+        nyr_local,
+        local_lx,
+        local_ly;
+        halo_x = HALO,
+        halo_y = HALO,
+        base_x = base_x_,
+        base_y = base_y_,
+    )
+
+    ghosts = ghost_quads(s)
+
+    cart_rank == 0 && println("Worker Cartesian grid: $(cart_dims[1])×$(cart_dims[2])")
+    println(
+        "Worker rank $cart_rank | coords=$(cart_coords) | local real mesh: $(nxr_local)×$(nyr_local)",
+    )
+    MPI.Barrier(cart_comm)
+
+    # ── DEC operators ─────────────────────────────────────────────────────────
+
+    dd0 = dual_derivative(Val(0), s)
+    ihs1 = inv_hodge_star(Val(1), s)
+    d1 = exterior_derivative(Val(1), s)
+    hs2 = hodge_star(Val(2), s)
+
+    # ── Initial condition ─────────────────────────────────────────────────────
+
+    u0 = zeros(FT, nquads(s))
+
+    center = [LX / 2, LY / 2]
+    covariance = [0.5, 0.1]
+    dist = MvNormal(center, covariance)
+
+    for ry in 1:nyqr(s), rx in 1:nxqr(s)
+        q = coord_to_quad(s, rx + hx(s), ry + hy(s))
+        dp = real_dual_point(s, rx, ry)
+        u0[q] = pdf(dist, [dp[1], dp[2]]) * 10.0
+    end
+
+    global_integral(u, s, comm) = MPI.Allreduce(sum(interior(Val(2), u, s)) * quad_area(s), +, comm)
+
+    mass_0 = global_integral(u0, s, cart_comm)
+    cart_rank == 0 && println("Initial mass: $(mass_0)")
+
+    # ── Plotting helpers ──────────────────────────────────────────────────────
+
+    cart_rank == 0 && rm(IMGDIR; recursive = true, force = true)
+    MPI.Barrier(cart_comm)
+    cart_rank == 0 && mkpath(IMGDIR)
+    MPI.Barrier(cart_comm)
+
+    local_max = maximum(interior(Val(2), u0, s))
+    global_max = MPI.Allreduce(local_max, max, cart_comm)
+
+    function plot_rank_slice(s, u, title_str, fname)
+        local_data = [u[coord_to_quad(s, x + hx(s), y + hy(s))] for y in 1:nyqr(s), x in 1:nxqr(s)]
+        fig = Figure(; size = (600, 500))
+        ax = Axis(fig[1, 1]; title = title_str, xlabel = "x", ylabel = "y")
+        hm = heatmap!(ax, local_data; colorrange = (0.0, global_max + eps()))
+        Colorbar(fig[1, 2], hm)
+        return save(fname, fig)
+    end
+
+    let title = "Rank $cart_rank | coords=$(cart_coords) | t=0.0"
+        fname = joinpath(
+            IMGDIR,
+            @sprintf("rank%03d_coords%d-%d_IC.png", cart_rank, cart_coords[1], cart_coords[2])
+        )
+        plot_rank_slice(s, u0, title, fname)
+    end
+
+    MPI.Barrier(cart_comm)
+
+    # ── RHS and callbacks ─────────────────────────────────────────────────────
+
+    step_counter = Ref(0)
+
+    function heat_rhs_mpi!(du, u, p, t)
+        _, k, _, dd0, ihs1, d1, hs2 = p
+        grad_u = dd0 * u
+        flux_dual = k .* grad_u
+        flux_primal = ihs1 * flux_dual
+        div_flux = d1 * flux_primal
+        laplacian_u = hs2 * div_flux
+        return du .= laplacian_u
+    end
+
+    p = (s, K_DIFFUSION, topo, dd0, ihs1, d1, hs2)
+
+    progress_cb =
+        FunctionCallingCallback(; func_everystep = true, func_start = false) do u, t, integrator
+            step_counter[] += 1
+            if step_counter[] % PRINT_EVERY_N_STEPS == 0
+                pct = 100.0 * integrator.t / integrator.sol.prob.tspan[2]
+                println(
+                    "Worker $cart_rank | step $(step_counter[]) | t = $(round(integrator.t, digits=4)) ($(round(pct, digits=1))%)",
+                )
+                flush(stdout)
+            end
+        end
+
+    exchange_cb = DiscreteCallback(
+        (u, t, integrator) -> true,
+        integrator -> exchange_quads_all!(integrator.u, ghosts, topo);
+        initialize = (c, u, t, integrator) -> exchange_quads_all!(u, ghosts, topo),
+        save_positions = (false, false),
+    )
+
+    # Save callback: fires at each saveat point, signals output then gathers.
+    save_cb = SavingCallback(
+        (u, t, integrator) -> begin
+            data = reshape(interior(Val(2), u, s), nxqr(s), nyqr(s))[:]
+            send_output!([data], stream, topo)
+        end;
+        saveat = T_START:SAVEAT:T_END,
+    )
+
+    cb = CallbackSet(exchange_cb, save_cb, progress_cb)
+
+    # ── ODE solve ─────────────────────────────────────────────────────────────
+
+    prob = ODEProblem(heat_rhs_mpi!, u0, (T_START, T_END), p)
+    cart_rank == 0 && println("Solving...")
+    sol = solve(prob, Tsit5(); saveat = SAVEAT, adaptive = false, dt = DT, callback = cb)
+    cart_rank == 0 && println("Solve complete.")
+
+    mass_f = global_integral(sol[end], s, cart_comm)
+    cart_rank == 0 && println("Final mass: $(mass_f)")
+
+    worker_to_output(SIGNAL_DONE, topo)
+
+    # ── Per-rank final plots ───────────────────────────────────────────────────
+
+    let title = "Rank $cart_rank | coords=$(cart_coords) | t=$(T_END)"
+        fname = joinpath(
+            IMGDIR,
+            @sprintf("rank%03d_coords%d-%d_final.png", cart_rank, cart_coords[1], cart_coords[2])
+        )
+        plot_rank_slice(s, sol[end], title, fname)
+    end
+
+    println("LEAVING WORKER RANK $world_rank")
+    MPI.Barrier(cart_comm)
+end
+
+println("PROCESS RANK $world_rank WAITING TO LEAVE")
+MPI.Barrier(world_comm)
+println("CROSSED THE BARRIER, RANK $world_rank")
+MPI.Finalize()
